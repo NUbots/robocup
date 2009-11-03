c1a4c6d575ae3e4f99875f9693864b617d18c4af
Jason Kulk <jason.kulk@gmail.com>
2009-11-03 16:44:11 +1100

    Added base sensors class

--- NUbot/NAOWebots/nao_soccer_player_blue.cpp
+++ NUbot/NAOWebots/nao_soccer_player_blue.cpp
@@ -14,33 +14,36 @@
 #include <cstring>
 #include <cstdlib>
 
+#include "../Sensors.h"
+
 using namespace std;
 
 int main(int argc, const char *argv[]) {
 
-  // find URBI port number (e.g. -p 54001) in controllerArgs
-  int port = -1;
-  for (int i = 1; i < argc; i++) {
-    if (strcmp(argv[i], "-p") == 0 && i + 1 < argc) {
-      port = atoi(argv[i + 1]);
-      break;
+    // find URBI port number (e.g. -p 54001) in controllerArgs
+    int port = -1;
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "-p") == 0 && i + 1 < argc) {
+          port = atoi(argv[i + 1]);
+          break;
+        }
     }
-  }
 
-  if (port == -1) {
-    cout << "Error: could not find port number in controllerArgs" << endl;
-    return 0;
-  }
+    if (port == -1) {
+        cout << "Error: could not find port number in controllerArgs" << endl;
+        return 0;
+    }
 
-  int playerID = port % 10;
-  Player *player = NULL;
+    int playerID = port % 10;
+    Player *player = NULL;
+    
+    cout << "Test print. Where does this go? It goes to the webots terminal!" << endl;
+    Sensors *sensors = new Sensors();
 
-  // choose GoalKepper/FieldPlayer role according to last digit of URBI port number
-  if (playerID == 0)
-    player = new GoalKeeper(playerID);
-  else
+    delete sensors;
+    // choose GoalKepper/FieldPlayer role according to last digit of URBI port number
     player = new FieldPlayer(playerID);
 
-  player->run();
-  delete player;
+    player->run();
+    delete player;
 }
