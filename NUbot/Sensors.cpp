6de076699e0607f199f696d959ec2c271b7e60ab
Jason Kulk <jason.kulk@gmail.com>
2009-11-03 16:42:16 +1100

    Blank base sensor class.
    This is still a work in progress.

--- NUbot/Sensors.cpp
+++ NUbot/Sensors.cpp
@@ -1,15 +1,50 @@
-/*
+/*! @file Sensors.cpp
+    @brief Partial implementation of base sensor class
 
+    @author Jason Kulk
+ 
+ Copyright (c) 2009 Jason Kulk
+ 
+ This file is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ This file is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with NUbot.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "Sensors.h"
+#include <iostream>
+using namespace std;
 
-
+/*! @brief Default constructor
+ */
 Sensors::Sensors()
 {
+    cout << "Sensors::Sensors" << endl;
 }
 
+/*! @breif Default destructor
+ */
 Sensors::~Sensors()
 {
 }
+
+/*void Sensors::test()
+{
+    cout << "Sensors::test()" << endl;
+    sensor_type blank;
+    blank.timestamp = 0;
+    vector<sensor_type> testsensors(10000, blank);
+    testsensors[3].timestamp = 12345.456;
+    testsensors[10].timestamp = 15606.12;
+    
+    cout << "testsensors[3].timestamp" << testsensors[3].timestamp << "testsensors[10].timestamp" << testsensors[10].timestamp << "whoknows" << testsensors[100].timestamp;
+}*/
 
--- NUbot/Sensors.h
+++ NUbot/Sensors.h
@@ -1,29 +1,54 @@
-/*
+/*! @file Sensors.h
+    @brief Declaration of a base sensor class to store sensor data in a platform independent way
 
+    @author Jason Kulk
+ 
+  Copyright (c) 2009 Jason Kulk
+ 
+    This file is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This file is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with NUbot.  If not, see <http://www.gnu.org/licenses/>.
 */
+
+#ifndef SENSORS_H
+#define SENSORS_H
 
 #include <vector>
 #include <pthread.h>
+#include <iostream>
 
 using namespace std;
 
+/*! @brief Container for a single sensor
+ */
 struct sensor_type 
 {
-    vector<float> data;
-    vector<float> sd;
-    bool isValid;
-    bool isCalculated;
-    long double timestamp;
-    pthread_mutex_t mutex;
+    vector<float> data;         //!< the sensor values
+    vector<float> sd;           //!< standard deviation for each sensor value sensor
+    bool isValid;               //!< true, if data is valid, false if not
+    bool isCalculated;          //!< true, if data has been calculated, false if the data is direct from a sensor
+    long double timestamp;      //!< the unix timestamp of the data
 };
 
+/*! @brief Base sensor storage class
+ */
 class Sensors
 {
 public:
     Sensors();
     ~Sensors();
+    
 private:
     
 };
 
-
+#endif
