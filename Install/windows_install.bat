ee3943e5f5283cfca4e530e83fadc8a1da4a8e6d
Steven <steven.nicklin@gmail.com>
2009-11-24 19:08:57 +1100

    Removed external source code from repository. Compressed versions from their respective websites are now included with a decompression script for windows. Need to make a script for linux / mac as well.

---
+++ Install/windows_install.bat
@@ -0,0 +1,7 @@
+%~dp07za465\7za.exe x %~dp0GLee-5.4.0-src.tar.gz -y
+%~dp07za465\7za.exe x %~dp0GLee-5.4.0-src.tar -o%~dp0..\NUview\gl\ -y
+del GLee-5.4.0-src.tar
+%~dp07za465\7za.exe x %~dp0zlib-1.2.3.tar.gz -y
+%~dp07za465\7za.exe x %~dp0zlib-1.2.3.tar -o%~dp0..\Tools\zlib\ -y
+del zlib-1.2.3.tar
+%~dp07za465\7za.exe x %~dp0diagona.zip -o%~dp0..\NUview\diagona\ -y
