fcb682f8d115b7565c4a8c63bca4b2ce9e2405b8
Aaron Wong <wongosaurus@gmail.com>
2009-11-25 13:11:25 +1100

    Steves Change to work on ALL Paths with spaces.

--- Install/windows_install.bat
+++ Install/windows_install.bat
@@ -1,17 +1,17 @@
 @ECHO off
 
 Rem --- Extract GLee library ---
-%~dp07za465\7za.exe x %~dp0GLee-5.4.0-src.tar.gz -y
-%~dp07za465\7za.exe x %~dp0GLee-5.4.0-src.tar -o%~dp0..\NUview\gl\ -y
+"%~dp07za465\7za.exe" x "%~dp0GLee-5.4.0-src.tar.gz" -y
+"%~dp07za465\7za.exe" x "%~dp0GLee-5.4.0-src.tar" -o"%~dp0..\NUview\gl\" -y
 del GLee-5.4.0-src.tar
 
 Rem --- Extract Icons library ---
-%~dp07za465\7za.exe x %~dp0diagona.zip -o%~dp0..\NUview\diagona\ -y
+"%~dp07za465\7za.exe" x "%~dp0diagona.zip" -o"%~dp0..\NUview\diagona\" -y
 
 Rem --- Extract zlib library ---
-%~dp07za465\7za.exe x %~dp0zlib-1.2.3.tar.gz -y
-%~dp07za465\7za.exe x %~dp0zlib-1.2.3.tar -y
-xcopy /E /I /Y zlib-1.2.3 %~dp0..\Tools\zlib
+"%~dp07za465\7za.exe" x "%~dp0zlib-1.2.3.tar.gz" -y
+"%~dp07za465\7za.exe" x "%~dp0zlib-1.2.3.tar" -y
+xcopy /E /I /Y zlib-1.2.3 "%~dp0..\Tools\zlib"
 del zlib-1.2.3.tar
 RMDIR /s /Q zlib-1.2.3
 
