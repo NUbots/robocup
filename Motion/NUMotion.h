8a2e2ff62fea4fdf762c97aef199cdfc67083746
Jason Kulk <jason.kulk@gmail.com>
2009-12-01 18:43:46 +1100

    Updated blank class to use actual sensor and actionator data storage classes

--- Motion/NUMotion.h
+++ Motion/NUMotion.h
@@ -4,64 +4,6 @@
     @class NUMotion
     @brief A module to provide motion
  
-    If I rename actions to jobs. Each job can have several tasks.
- 
-    I will have a JobType, and a JobID
-
-    BODYJOB:
-        Job: Stand, Kick, Save
-        time and position
-            KICK: distance and bearing to kick the object
-            SAVE: nothing extra, a dive should emerge if a distance needs to be covered in a small amount of time.
- 
-    HEADJOB:
-        Job: Track, Nod, Pan
-        time and position
- 
-    LIGHTJOB:
-        Job: Chest, LFoot, RFoot, LEar etc
-        time and value
- 
-    CAMERAJOB:
-        Job: Exposure, Gain etc
-        time and value
- 
-    SOUNDJOB:
-        Job: Playing, Set, Initial etc
-        time
- 
-    Action
-        -- BodyAction
-        -- HeadAction
-        -- LightAction
-        -- CameraAction
-        -- SoundAction
-        Every action has a
-            - Type
-            - Job
-            - Time
-        Some actions have more data
-            - a Value or position
-            - kick distance and kick bearing
- 
-    if (action.Type == Action::Body)
-        position = action.getPosition()         // get the position where the action is to be performed
-        time = action.getTime()               // get the time at which to perform the action
-        job = action.getJob()
-        
- 
-TODO: I need to rethink this a little. Each class can not have a different enum to represent JobType!.
- Option 1. Put all job types in Action.
- Option 2. Avoid needing enum types by providing alot of construtors/static functions.
-        Action* action = BodyAction::newKickJob(time, position, kicktarget);        // I don't have a problem with this
-        action->jobType;    // how do I know if it is a stand, kick or save ??? I need a get method anyway.
- Option 3. I could have a crap load of inheritance.
-        Action* action = new KickJob(position, kicktarget);
-        still how do I know what type it is!!!!!!!!!
- Option 4. I could use dynamic_cast or typeid. However, this seem to be a bad idea
- Option 5. Just directly access the JobType variable (This doesn't work because I can only compare enums of the same type.
- 
- 
     @author Jason Kulk
  
   Copyright (c) 2009 Jason Kulk
@@ -84,20 +26,8 @@ TODO: I need to rethink this a little. Each class can not have a different enum 
 #define NUMOTION_H
 
 #include "Behaviour/JobList.h"
-
-class ActuatorCommands
-{
-public:
-    ActuatorCommands() {};
-    ~ActuatorCommands() {};
-};
-
-class BodyData
-{
-public:
-    BodyData() {};
-    ~BodyData() {};
-};
+#include "NUPlatform/NUSensors/NUSensorsData.h"
+#include "NUPlatform/NUActionators/NUActionatorsData.h"
 
 class NUMotion
 {
@@ -105,7 +35,7 @@ public:
     NUMotion();
     ~NUMotion();
     
-    ActuatorCommands* process(BodyData* data);
+    void process(NUSensorsData* data, NUActionatorsData* actions);
     void process(JobList jobs);
 protected:
 private:
