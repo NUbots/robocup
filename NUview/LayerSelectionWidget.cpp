6d75b4972fcc0ec814303bd843ba3683dd8c0295
Steven <steven.nicklin@gmail.com>
2009-12-03 18:40:03 +1100

    Added widget to configure layer displays in the image view windows.

---
+++ NUview/LayerSelectionWidget.cpp
@@ -0,0 +1,5 @@
+#include "LayerSelectionWidget.h"
+
+LayerSelectionWidget::LayerSelectionWidget()
+{
+}
---
+++ NUview/LayerSelectionWidget.h
@@ -0,0 +1,12 @@
+#ifndef LAYERSELECTIONWIDGET_H
+#define LAYERSELECTIONWIDGET_H
+
+#include <QWidget>
+
+class LayerSelectionWidget : public QWidget
+{
+public:
+    LayerSelectionWidget();
+};
+
+#endif // LAYERSELECTIONWIDGET_H
