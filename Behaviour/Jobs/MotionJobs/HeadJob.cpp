31392223610a1f1e9badf1f2d3209de5f59d7600
Jason Kulk <jason.kulk@gmail.com>
2009-12-10 22:41:51 +1100

    Added a generic Head job to move the head to a desired position.

---
+++ Behaviour/Jobs/MotionJobs/HeadJob.cpp
@@ -0,0 +1,57 @@
+/*! @file HeadJob.cpp
+    @brief Implementation of HeadJob class
+
+    @author Jason Kulk
+ 
+ Copyright (c) 2009 Jason Kulk
+ 
+ This file is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ This file is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with NUbot.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "HeadJob.h"
+
+HeadJob::HeadJob(double time, const vector<float>& position) : MotionJob(Job::MOTION_WALK)
+{
+    m_job_time = time;     
+    m_head_position = position;
+}
+
+/*! @brief WalkJob destructor
+ */
+HeadJob::~HeadJob()
+{
+    m_head_position.clear();
+}
+
+/*! @brief Sets the position for the head to point
+ 
+    You should only need to use this function if you are recycling the one job 
+    (ie. I provide this function if you are worried about creating a new job every time)
+    
+    @param newposition the new position for the head job [x(cm), y(cm), theta(rad)]
+ */
+void HeadJob::setPosition(double time, const vector<float>& newposition)
+{
+    m_job_time = time;
+    m_head_position = newposition;
+}
+
+/*! @brief Gets the position of the head job
+    @param position parameter that will be updated with the point we want to head
+ */
+void HeadJob::getPosition(double& time, vector<float>& position)
+{
+    time = m_job_time;
+    position = m_head_position;
+}
---
+++ Behaviour/Jobs/MotionJobs/HeadJob.h
@@ -0,0 +1,52 @@
+/*! @file HeadJob.h
+    @brief Declaration of HeadJob class.
+ 
+    @class HeadJob
+    @brief A class to encapsulate jobs issued for the head module. This particular job just moves the 
+           head to the given angles.
+ 
+    @author Jason Kulk
+ 
+  Copyright (c) 2009 Jason Kulk
+ 
+    This file is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This file is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with NUbot.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef HEADJOB_H
+#define HEADJOB_H
+
+#include "../MotionJob.h"
+#include <vector>
+using namespace std;
+
+class HeadJob : public MotionJob
+{
+public:
+    HeadJob(double time, const vector<float>& position);
+    ~HeadJob();
+    
+    void setPosition(double time, const vector<float>& newposition);
+    void getPosition(double& time, vector<float>& position);
+    
+    /*virtual void summaryTo(ostream& output);
+    virtual void csvTo(ostream& output);
+    
+    virtual ostream& operator<< (ostream& output);
+    virtual istream& operator>> (istream& input);*/
+private:
+    vector<float> m_head_position;                 //!< the head position [yaw (rad), pitch (rad), roll (rad)]
+};
+
+#endif
+
