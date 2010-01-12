cee058e35519f180aea630791a8f9ee1e37b6e51
Jason Kulk <jason.kulk@gmail.com>
2010-01-12 18:01:18 +1100

    Added a little function to make the debug log be prefixed with the player's number so that multiple robots dont share the same log in webots.

--- NUPlatform/Platforms/NAOWebots/nao_soccer_player_blue.cpp
+++ NUPlatform/Platforms/NAOWebots/nao_soccer_player_blue.cpp
@@ -10,13 +10,31 @@
 
 #include "NUbot.h"
 
+#include <sstream>
 using namespace std;
-
 ofstream debug;
+
+int getPlayerNumber(int argc, const char *argv[])
+{
+    int port = -1;
+    for (int i = 1; i < argc; i++) 
+    {
+        if (strcmp(argv[i], "-p") == 0 && i + 1 < argc) {
+            port = atoi(argv[i + 1]);
+            break;
+        }
+    }
+    
+    if (port == -1) 
+        cout << "Error: could not find port number in controllerArgs" << endl;
+    return (port % 10) + 1;
+}
 
 int main(int argc, const char *argv[]) 
 {
-    debug.open("debug.log");
+    stringstream filename;
+    filename << getPlayerNumber(argc, argv) << "debug.log";
+    debug.open(filename.str().c_str());    // I need to use a different name for each robot!
     NUbot* nubot = new NUbot(argc, argv);
     nubot->run();
     delete nubot;
