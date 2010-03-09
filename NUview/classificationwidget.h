115318205ae1d232f7e476f6709a5c1ee165756c
Steven <steven.nicklin@gmail.com>
2010-03-10 00:48:41 +1100

    Update to fit new Pixel.h union. Greatly simplified the alternate colour selection.

--- NUview/classificationwidget.h
+++ NUview/classificationwidget.h
@@ -10,7 +10,7 @@
 #include <QDockWidget>
 #include <vector>
 #include "Vision/ClassificationColours.h"
-#include "Tools/Image/pixels.h"
+#include "Tools/Image/Pixel.h"
 class QComboBox;
 class QLabel;
 class QSpinBox;
@@ -51,39 +51,84 @@ public:
       @breif Returns the currently selected colour.
       @return The selected colour.
       */
-    pixels::Pixel getCurrentColour();
+    Pixel getCurrentColour();
 
-    /*!
-      Returns the currently selected colour in the specified colour format.
-      @return The selected colour.
-      */
-    pixels::Pixel getCurrentColourInColourModel(int colourSpace);
-
-    static pixels::Pixel convertToColourSpace(pixels::Pixel, int sourceColourSpace, int desiredColourSpace);
+    static Pixel convertToColourSpace(Pixel, int sourceColourSpace, int desiredColourSpace);
 
      /*!
       Returns the currently selected colour.
       @return The selected colour.
       */
     ClassIndex::Colour getColourLabel();
+    /*!
+     Conver a colour space to a YCrCb Pixel.
+     @param chan0 First channel of the colour.
+     @param chan1 Second channel of the colour.
+     @param chan2 Third channel of the colour.
+     @param originalColourSpace The colour space of the above 3 channels.
+     @return The selected colour in YCrCb format.
+     */
+    Pixel convertToYCbCr(unsigned char chan0,unsigned char chan1,unsigned char chan2, ColourSpace originalColour);
+
+    /*!
+     Conver a YCbCr Pixel to channels of the desired colour space.
+     @param colour The colour to convert.
+     @param chan0 First channel of the coverted colour result.
+     @param chan1 Second channel of the converted colour result.
+     @param chan2 Third channel of the converted colour result.
+     @param originalColourSpace The colour space to which the colour is to be converted.
+     @return The selected colour in YCrCb format.
+     */
+    void convertfromYCbCr(Pixel colour, unsigned char& chan0, unsigned char& chan1, unsigned char& chan2, ColourSpace newColour);
 
 public slots:
+    /*!
+     Open a lookup table
+     */
     void doOpen();
+    /*!
+     Save current lookup table.
+     */
     void doSaveAs();
-    void updateColourSpace(int newColourSpace);
+    /*!
+     Sets the current selection colour space.
+     @param newColourSpace THe new colour space for selection.
+     */
+    void setColourSpace(int newColourSpace);
+    /*!
+     Draw the selected colour preview square.
+     */
     void drawSelectedColour();
-    void setColour(pixels::Pixel newColour, int colourSpace);
-    std::vector<pixels::Pixel> getSelectedColours(int colourSpace);
+    /*!
+     Set the selected colour.
+     @param newColour THe new colour to be selected.
+     */
+    void setColour(Pixel newColour);
+    /*!
+     Get the selected colours based on the central colour and the bounds.
+     @return A vector of the selected colours.
+     */
+    std::vector<Pixel> getSelectedColours();
+    /*!
+     Perform an autosave of the lookup table to autosave.lut.
+     */
     void PerformAutosave();
-    void selectionChanged();
+    /*!
+     Set all of the bounds to a common magnitude.
+     @param newBounds The new magnitude of all of the bounds.
+     */
     void setAllBoundaries(int newBounds);
+    /*!
+     Change the auto soft colour flag.
+     @param The state of the auto soft colour checkbox.
+     */
     void autoSoftColourStateChanged(int newState);
 
 signals:
     void openLookupTableFile(QString fileName);
     void saveLookupTableFile(QString fileName);
     void displayStatusBarMessage(QString message, int timeout=0);
-    void newSelection();
+    void selectionChanged();
     void autoSoftColourChanged(bool);
 private:
     ColourSpace currentColourSpace;
