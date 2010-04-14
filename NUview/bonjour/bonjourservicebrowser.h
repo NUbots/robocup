f0742c8cec6377debb790e7392819da9ed3ae5f6
Steven <steven.nicklin@gmail.com>
2010-04-14 15:19:01 +1000

    Changed included file name.

--- NUview/bonjour/bonjourservicebrowser.h
+++ NUview/bonjour/bonjourservicebrowser.h
@@ -30,7 +30,7 @@ ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #define BONJOURSERVICEBROWSER_H
 
 #include <QtCore/QObject>
-#include "bonjourIncludeHack.h"
+#include "bonjourIncludeDataTypes.h"
 #include <dns_sd.h>
 #include "bonjourrecord.h"
 
