170c9d68fdf4b8018a20b1d1626df5711a4819d3
Jason Kulk <jason.kulk@gmail.com>
2010-04-20 17:34:44 +1000

    Added more sounds to NUSounds.h

--- NUPlatform/NUActionators/NUSounds.h
+++ NUPlatform/NUActionators/NUSounds.h
@@ -26,6 +26,27 @@
 
 namespace NUSounds 
 {
+    std::string PRE_INITIAL = std::string("pre_initial.wav");
+    std::string INITIAL = std::string("initial.wav");
+    std::string READY = std::string("ready.wav");
+    std::string SET = std::string("set.wav");
+    std::string PLAYING = std::string("playing.wav");
+    std::string FINISHED = std::string("finished.wav");
+    std::string PENALISED = std::string("penalised.wav");
+    std::string REQUEST_PICK_UP = std::string("request_pick_up.wav");
+    std::string SUBSTITUTE = std::string("substitute.wav");
+    std::string RED_TEAM = std::string("red_team.wav");
+    std::string BLUE_TEAM = std::string("blue_team.wav");
+    std::string NUBOT1 = std::string("nubot1.wav");
+    std::string NUBOT2 = std::string("nubot2.wav");
+    std::string NUBOT3 = std::string("nubot3.wav");
+    std::string NUBOT4 = std::string("nubot4.wav");
+    std::string NUBOT5 = std::string("nubot5.wav");
+    std::string NUBOT6 = std::string("nubot6.wav");
+    std::string NUBOT7 = std::string("nubot7.wav");
+    std::string NUBOT8 = std::string("nubot8.wav");
+    std::string NUBOT9 = std::string("nubot9.wav");
+    std::string NUBOT10 = std::string("nubot10.wav");
     std::string SEG_FAULT = std::string("seg_fault.wav");
     std::string UNHANDLED_EXCEPTION = std::string("unhandled_exception.wav");
 }
