3f89e92bd2a60283ddfc57193d359892dbd5ea78
Steven <steven.nicklin@gmail.com>
2010-04-30 10:56:16 +1000

    *Re-Ordered variables *Added name

--- Kinematics/EndEffector.cpp
+++ Kinematics/EndEffector.cpp
@@ -1,17 +1,29 @@
 #include "EndEffector.h"
 #include "debug.h"
 
-EndEffector::EndEffector()
+EndEffector::EndEffector(const Matrix& startTrans, const std::vector<Link>& endEffectorlinks, const Matrix& endTrans, const std::string& effectorName):
+        m_startTransform(startTrans), m_links(endEffectorlinks), m_endTransform(endTrans), m_name(effectorName)
 {
 }
 
 Matrix EndEffector::CalculateTransform(std::vector<float> jointValues)
 {
-    Matrix result(startTransform);
-    if(jointValues.size() != links.size())
+    Matrix result(m_startTransform);
+    if(jointValues.size() != m_links.size())
     {
-        errorlog << "EndEffector::CalculateTransform - Joint values do not match joints." << std::endl;
+        errorlog << "EndEffector::CalculateTransform - Joint values do not match links." << std::endl;
     }
 
+    // Make iterators
+    std::vector<Link>::const_iterator endLinks = m_links.end();
+    std::vector<Link>::iterator currLink = m_links.begin();
+    std::vector<float>::iterator currJointAngle = jointValues.begin();
+    while (currLink != endLinks)
+    {
+        result = result * currLink->calculateTransform(*currJointAngle);
+        ++currLink;
+        ++currJointAngle;
+    }
+    result = result * m_endTransform;
     return result;
 }
--- Kinematics/EndEffector.h
+++ Kinematics/EndEffector.h
@@ -6,13 +6,18 @@
 
 class EndEffector
 {
-    Matrix startTransform;
-    std::vector<Link> links;
-    Matrix endTransform;
+    Matrix m_startTransform;
+    std::vector<Link> m_links;
+    Matrix m_endTransform;
+    std::string m_name;
 
 public:
-    EndEffector();
+    EndEffector(const Matrix& startTrans,
+                const std::vector<Link>& endEffectorlinks,
+                const Matrix& endTrans,
+                const std::string& effectorName = std::string("Unknown"));
     Matrix CalculateTransform(std::vector<float> jointValues);
+    std::string Name() {return m_name;};
 };
 
 #endif // ENDEFFECTOR_H
