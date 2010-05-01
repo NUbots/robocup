d04ac2bd93acdea32b036de2a3ea78e73c2b7415
Steven <steven.nicklin@gmail.com>
2010-05-01 18:32:15 +1000

    Added error checking on the lenght of the joints.

--- Kinematics/EndEffector.cpp
+++ Kinematics/EndEffector.cpp
@@ -11,18 +11,21 @@ Matrix EndEffector::CalculateTransform(std::vector<float> jointValues)
     Matrix result(m_startTransform);
     if(jointValues.size() != m_links.size())
     {
-        errorlog << "EndEffector::CalculateTransform - Joint values do not match links." << std::endl;
+        errorlog << "EndEffector::CalculateTransform - Joint values do not match links. ";
+        errorlog << m_links.size() << " Links but only " << jointValues.size() << " joint values given." << std::endl;
     }
-
-    // Make iterators
-    std::vector<Link>::const_iterator endLinks = m_links.end();
-    std::vector<Link>::iterator currLink = m_links.begin();
-    std::vector<float>::iterator currJointAngle = jointValues.begin();
-    while (currLink != endLinks)
+    else
     {
-        result = result * currLink->calculateTransform(*currJointAngle);
-        ++currLink;
-        ++currJointAngle;
+        // Make iterators
+        std::vector<Link>::const_iterator endLinks = m_links.end();
+        std::vector<Link>::iterator currLink = m_links.begin();
+        std::vector<float>::iterator currJointAngle = jointValues.begin();
+        while (currLink != endLinks)
+        {
+            result = result * currLink->calculateTransform(*currJointAngle);
+            ++currLink;
+            ++currJointAngle;
+        }
     }
     result = result * m_endTransform;
     return result;
