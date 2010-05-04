5566e1f74835a98bbcefa2634c44abe54702d023
Jason Kulk <jason.kulk@gmail.com>
2010-05-04 18:40:42 +1000

    Fixed initialisation in Kinematics::Link and PeriodicThread classes.

--- Kinematics/Link.cpp
+++ Kinematics/Link.cpp
@@ -3,6 +3,8 @@ using namespace TransformMatrices;
 Link::Link(const TransformMatrices::DHParameters& linkParameters, const std::string& linkName):
         m_name(linkName), m_parameters(linkParameters)
 {
+    m_bufferedAngle = 0.0f;
+    m_bufferedTransform = ModifiedDH(m_parameters, m_bufferedAngle);
 }
 
 
