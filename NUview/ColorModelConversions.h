9af354713e646e9aa417397b0b5668b4b9ba98c7
Steven <steven.nicklin@gmail.com>
2010-05-10 15:42:01 +1000

    Fixed compiler warning

--- NUview/ColorModelConversions.h
+++ NUview/ColorModelConversions.h
@@ -177,8 +177,8 @@ public:
     float r, g, b, hDeg;
     float f, p, q, t;
     hDeg = 360.0 / 255.0 * (float)H;
-    float hfloor = floor(hDeg / 60.0);
-    int hi = (int)(fmod(hfloor, 6.0));
+    float hfloor = floor(hDeg / 60.0f);
+    int hi = (int)(fmod(hfloor, 6.0f));
     f = (hDeg / 60 - hfloor);
     p = V * (255 - S) / 255.0;
     q = V * (255 - f*S) / 255.0;
