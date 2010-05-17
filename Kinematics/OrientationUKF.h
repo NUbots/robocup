1fc1ee0f83ce4b2a7c711421f667a855e4cdf971
Steven <steven.nicklin@gmail.com>
2010-05-17 15:16:25 +1000

    Changed interface of ukf to prevent unnneded data type conversions.

--- Kinematics/OrientationUKF.h
+++ Kinematics/OrientationUKF.h
@@ -13,16 +13,11 @@ public:
         pitchGyroOffset,
         rollAngle,
         rollGyroOffset,
-        //xAcceleration,
-        //yAcceleration,
-        //zAcceleration,
         numStates
     };
-    void initialise(double time, float pitchGyro, float rollGyro, float accX, float accY, float accZ);
-    void TimeUpdate(float pitchGyroReading, float rollGyroReading, double timestamp);
-    void MeasurementUpdate(const std::vector<float>& accelerations, bool validKinematics, const std::vector<float> kinematicsOrientation);
-    void AccelerometerMeasurementUpdate(float xAccel, float yAccel, float zAccel);
-    void KinematicsMeasurementUpdate(float pitchMeasurement, float rollMeasurement);
+    void initialise(double time, const std::vector<float>& gyroReadings, const std::vector<float>& accelerations);
+    void TimeUpdate(const std::vector<float>& gyroReadings, double timestamp);
+    void MeasurementUpdate(const std::vector<float>& accelerations, bool validKinematics, const std::vector<float>& kinematicsOrientation);
     bool Initialised(){return m_initialised;};
 
 private:
