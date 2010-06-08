f415a80c683cf37cb43e25e7252a42aa9016473d
Jason Kulk <jason.kulk@gmail.com>
2010-06-08 20:22:46 +1000

    Updated the PlayingState so that the right eye leds display the lost state.

--- Behaviour/Soccer/PlayingState.cpp
+++ Behaviour/Soccer/PlayingState.cpp
@@ -42,16 +42,19 @@ PlayingState::PlayingState(SoccerProvider* provider) : SoccerFSMState(provider)
     
     m_state = m_chase_state;
     
-    m_chase_led_indices.push_back(0);
-    m_chase_led_indices.push_back(7);
-    m_lost_led_indices.push_back(3);
-    m_lost_led_indices.push_back(4);
+    m_chase_led_indices.push_back(3);
+    m_chase_led_indices.push_back(4);
+    m_lost_led_indices.push_back(0);
+    m_lost_led_indices.push_back(7);
     m_led_on = vector<vector<float> >(1, vector<float>(3,1.0f));
     m_led_off = vector<vector<float> >(1, vector<float>(3,0.0f));
     m_led_red = m_led_off;
     m_led_red[0][0] = 1;
     m_led_green = m_led_off;
     m_led_green[0][1] = 1;
+    m_led_yellow = m_led_off;
+    m_led_yellow[0][0] = 1;
+    m_led_yellow[0][1] = 1;
 }
 
 PlayingState::~PlayingState()
@@ -80,13 +83,17 @@ void PlayingState::doStateCommons()
     else
         m_actions->addLeds(NUActionatorsData::RightEyeLeds, m_chase_led_indices, m_actions->CurrentTime, m_led_off);
     
-    if (m_state == m_ball_is_lost_state)
+    // set the right eyes to indicate lost states
+    bool balllost = m_field_objects->mobileFieldObjects[FieldObjects::FO_BALL].lost();
+    bool selflost = m_field_objects->self.lost();
+    if (balllost and selflost)
+        m_actions->addLeds(NUActionatorsData::RightEyeLeds, m_lost_led_indices, m_actions->CurrentTime, m_led_yellow);
+    else if (balllost)
         m_actions->addLeds(NUActionatorsData::RightEyeLeds, m_lost_led_indices, m_actions->CurrentTime, m_led_red);
-    else if (m_field_objects->self.lost() or m_state == m_im_lost_state)
+    else if (selflost)
         m_actions->addLeds(NUActionatorsData::RightEyeLeds, m_lost_led_indices, m_actions->CurrentTime, m_led_green);
     else
         m_actions->addLeds(NUActionatorsData::RightEyeLeds, m_lost_led_indices, m_actions->CurrentTime, m_led_off);
-        
 }
 
 BehaviourFSMState* PlayingState::nextStateCommons()
