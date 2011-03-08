1d7097a1a5e0dc71b8027d3213203fa2b0e0cee8
Steven Nicklin <steven@Stevens-MacBook-Pro.local>
2011-03-08 17:58:11 +1100

    Added missing includes for when vision is enabled.

--- Behaviour/WalkOptimisation/WalkOptimisationState.h
+++ Behaviour/WalkOptimisation/WalkOptimisationState.h
@@ -32,6 +32,8 @@
 #include "nubotconfig.h"
 #include "debug.h"
 #include "debugverbositybehaviour.h"
+#include "Infrastructure/Jobs/MotionJobs/HeadTrackJob.h"
+#include "Infrastructure/Jobs/MotionJobs/HeadPanJob.h"
 
 class WalkOptimisationState : public BehaviourState
 {
