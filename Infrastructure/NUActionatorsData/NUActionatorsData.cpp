e621a04b66ef51c4e64971fc9a17d94fee28047b
Jason Kulk <jason.kulk@gmail.com>
2011-08-18 19:45:26 +1000

    Fixed the interpolation in Webots. It seems that the motionthread can be called twice with the same timestamp; this would break it.
    It would be strange if this also fixes the Darwin interpolation.

--- Infrastructure/NUActionatorsData/NUActionatorsData.cpp
+++ Infrastructure/NUActionatorsData/NUActionatorsData.cpp
@@ -359,7 +359,7 @@ void NUActionatorsData::getNextSounds(vector<string>& sounds)
 float NUActionatorsData::interpolate(const double& time, const float& current, const float& target)
 {
     float dT = CurrentTime - PreviousTime;
-    if (dT > 0 and dT < 500)
+    if (dT > 0)
     {
         if (time - CurrentTime > dT)
             return current + ((target - current)/(time - CurrentTime))*dT;
@@ -367,7 +367,7 @@ float NUActionatorsData::interpolate(const double& time, const float& current, c
             return target;
     }
     else
-        return target;
+        return current;
 }
 
 /*! @brief Returns true if a member named name belongs to the group
