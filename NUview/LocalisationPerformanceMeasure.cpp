244baca9be6460b41b2cec9f58fab7b03ec53145
Steven Nicklin <steven@Stevens-MBP.local>
2011-11-03 14:08:15 +1100

    Fixed error where error values were not copied.

--- NUview/LocalisationPerformanceMeasure.cpp
+++ NUview/LocalisationPerformanceMeasure.cpp
@@ -8,4 +8,7 @@ LocalisationPerformanceMeasure::LocalisationPerformanceMeasure()
 LocalisationPerformanceMeasure::LocalisationPerformanceMeasure(const LocalisationPerformanceMeasure& source)
 {
     m_processing_time = source.m_processing_time;
+    m_err_x = source.m_err_x;
+    m_err_y = source.m_err_y;
+    m_err_heading = source.m_err_heading;
 }
