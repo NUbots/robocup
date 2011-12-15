f7aab0731fcda858101a4c4932d20f473f55ca4b
Steven Nicklin <steven@Stevens-MBP.local>
2011-12-15 11:19:21 +1100

    Tools for the kalman filter Added. Need to be added to.

---
+++ Tools/KFTools.cpp
@@ -0,0 +1,18 @@
+#include "KFTools.h"
+
+KFTools::KFTools()
+{
+}
+
+static Matrix KFTools::CovarianceIntersectionMean(Matrix meanA, Matrix CovarianceA, Matrix meanB, Matrix CovarianceB, float weight)
+{
+    Matrix result;
+    //result =
+    return result;
+}
+
+static Matrix KFTools::CovarianceIntersectionCovariance(Matrix meanA, Matrix CovarianceA, Matrix meanB, Matrix CovarianceB, float weight)
+{
+    Matrix result;
+    return result;
+}
