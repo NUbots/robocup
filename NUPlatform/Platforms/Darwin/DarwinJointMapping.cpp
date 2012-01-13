346e16fd8d6ed2eecee5581475803c2f0e2f1142
Steven Nicklin <steven@Stevens-MBP.local>
2012-01-14 01:29:24 +1100

    Modified class to perform the entire conversion between raw motor values and joint space angles. The code now uses pre-calculated tables rather than the switch statement used previously.

--- NUPlatform/Platforms/Darwin/DarwinJointMapping.cpp
+++ NUPlatform/Platforms/Darwin/DarwinJointMapping.cpp
@@ -1,57 +1,150 @@
 #include "DarwinJointMapping.h"
+#include "Tools/Math/General.h"
+#include <MX28.h>		//Darwin Motors
 
+
+/*! @brief Constructs a DarwinJointMapping class.
+
+  The offset and muliplier tables are initialised.
+ */
 DarwinJointMapping::DarwinJointMapping()
 {
+    float temp_off[] =
+    {
+        0.0f,           // [0]  HEAD_PITCH
+        0.0f,           // [1]  HEAD_YAW
+        -0.7853981f,    // [2]  L_SHOULDER_ROLL
+        1.5707963f,     // [3]  L_SHOULDER_PITCH
+        -1.5707963f,    // [4]  L_ELBOW
+        0.7853981f,     // [5]  R_SHOULDER_ROLL
+        -1.5707963f,    // [6]  R_SHOULDER_PITCH
+        1.5707963,      // [7]  R_ELBOW
+        0.0f,           // [8]  L_HIP_ROLL
+        0.0f,           // [9]  L_HIP_PITCH
+        0.0f,           // [10] L_HIP_YAW
+        0.0f,           // [11] L_KNEE
+        0.0f,           // [12] L_ANKLE_ROLL
+        0.0f,           // [13] L_ANKLE_PITCH
+        0.0f,           // [14] R_HIP_ROLL
+        0.0f,           // [15] R_HIP_PITCH
+        0.0f,           // [16] R_HIP_YAW
+        0.0f,           // [17] R_KNEE
+        0.0f,           // [18] R_ANKLE_ROLL
+        0.0f,           // [19] R_ANKLE_PITCH
+    };
 
-    float temp_servo_Offsets[] = 		{	0.0, 	0.0, \
-                                                    //JointID::ID_L_SHOULDER_ROLL, JointID::ID_L_SHOULDER_PITCH, JointID::ID_L_ELBOW,
-                                                    -0.7853981, 1.5707963, -1.5707963, \
-                                                    //JointID::ID_R_SHOULDER_ROLL, JointID::ID_R_SHOULDER_PITCH, JointID::ID_R_ELBOW,
-                                                    0.7853981, -1.5707963, 1.5707963, \
-                                                    //JointID::ID_L_HIP_ROLL, JointID::ID_L_HIP_PITCH, JointID::ID_L_HIP_YAW,
-                                                    0.0,	0.0,	0.0,	\
-                                                    //JointID::ID_L_KNEE, JointID::ID_L_ANKLE_ROLL, JointID::ID_L_ANKLE_PITCH,
-                                                    0.0,	0.0,	0.0,	\
-                                                    //JointID::ID_R_HIP_ROLL, JointID::ID_R_HIP_PITCH, JointID::ID_R_HIP_YAW,
-                                                    0.0,	0.0,	0.0,	\
-                                                    //JointID::ID_R_KNEE, JointID::ID_R_ANKLE_ROLL, JointID::ID_R_ANKLE_PITCH};
-                                                    0.0,	0.0,	0.0};
-    m_servo_offsets = std::vector<float>(temp_servo_Offsets, temp_servo_Offsets + sizeof(temp_servo_Offsets)/sizeof(*temp_servo_Offsets));
+    char temp_mult[] =
+    {
+        -1,             // [0]  HEAD_PITCH
+        1,              // [1]  HEAD_YAW
+        -1,             // [2]  L_SHOULDER_ROLL
+        1,              // [3]  L_SHOULDER_PITCH
+        1,              // [4]  L_ELBOW
+        -1,             // [5]  R_SHOULDER_ROLL
+        -1,             // [6]  R_SHOULDER_PITCH
+        -1,             // [7]  R_ELBOW
+        -1,             // [8]  L_HIP_ROLL
+        -1,             // [9]  L_HIP_PITCH
+        -1,             // [10] L_HIP_YAW
+        -1,             // [11] L_KNEE
+        1,              // [12] L_ANKLE_ROLL
+        1,              // [13] L_ANKLE_PITCH
+        -1,             // [14] R_HIP_ROLL
+        1,              // [15] R_HIP_PITCH
+        -1,             // [16] R_HIP_YAW
+        1,              // [17] R_KNEE
+        1,              // [18] R_ANKLE_ROLL
+        -1,             // [19] R_ANKLE_PITCH
+    };
+
+    m_offsets = std::vector<float>(temp_off, temp_off + sizeof(temp_off)/sizeof(*temp_off));
+    m_multipliers = std::vector<char>(temp_mult, temp_mult + sizeof(temp_mult)/sizeof(*temp_mult));
     return;
 }
 
-float DarwinJointMapping::convertJointPosition(unsigned int id, float position)
+/*! @brief Converts a motor angle in radians into a Robotis MX28 motor value.
+
+  @param radian The motor angle in radians.
+  @return The Robotis MX28 motor value corresponding to the radian value parameter.
+ */
+int DarwinJointMapping::Radian2Value(float radian)
 {
-    float value;
-    switch(id)
-    {
-        // Values that require negation.
-        case 0:     // Head Pitch
-        case 2:     // Left Shoulder Roll
-        case 5:     // Right Shoulder Roll
-        case 6:     // Right Shoulder Pitch
-        case 7:     // Right Elbow Pitch
-        case 8:     // Left Hip Roll
-        case 9:     // Left Hip Pitch
-        case 10:    // Left Hip Yaw
-        case 11:    // Right Knee Pitch
-        case 14:    // Right Hip Roll
-        case 16:    // Right Ankle Pitch
-        case 19:    // Right Ankle Pitch
-           value = -position;
-           break;
-        // All other values do not require negation
-        default:
-            value = position;
-    }
+    static const float RATIO_RADIAN2VALUE = Robot::MX28::MAX_VALUE / (2*mathGeneral::PI);
+    int value = (int)(radian*RATIO_RADIAN2VALUE)+Robot::MX28::CENTER_VALUE;
     return value;
 }
 
-std::vector<float> DarwinJointMapping::convertJointPositionValues(std::vector<float> joints)
+/*! @brief Converts a Robotis MX28 motor value into a motor angle in radians.
+
+  @param value The Robotis MX28 motor value.
+  @return The motor angle in radians corresponding to the motor value parameter.
+ */
+float DarwinJointMapping::Value2Radian(int value)
 {
-    for (unsigned int id = 0; id < joints.size(); ++id)
+    static const float RATIO_VALUE2RADIAN = (2*mathGeneral::PI) / Robot::MX28::MAX_VALUE;
+    float radian = (float)(value-Robot::MX28::CENTER_VALUE)*RATIO_VALUE2RADIAN;
+    return radian;
+}
+
+/*! @brief  Converts a joint angle in radians within the NUbots joint space into a Robotis MX28 motor value.
+
+  The NUbots joint space is the standard joint angle definitions constant between all robots. In order to
+  convert the angles into this space an offset and multiplier are applied to each angle.
+
+  @param id The motor ID within the local joint list.
+  @param value The Robotis MX28 motor value.
+  @return The motor value with the offset and multipler values removed, corresponding to the joint angle parameter.
+ */
+int DarwinJointMapping::joint2raw(unsigned int id, float joint) const
+{
+    int raw = Radian2Value(m_multipliers[id] * joint - m_offsets[id]);
+    return raw;
+}
+
+/*! @brief  Converts a Robotis MX28 motor value into a joint angle in radians within the NUbots joint space.
+
+  The NUbots joint space is the standard joint angle definitions constant between all robots. In order to
+  convert the angles into this space an offset and multiplier are applied to each angle.
+
+  @param id The motor ID within the local joint list.
+  @param value The raw Robotis MX28 motor value.
+  @return The joint angle with the offset and multipler values applied, corresponding to the motor value parameter.
+ */
+float DarwinJointMapping::raw2joint(unsigned int id, int raw) const
+{
+    float joint = (Value2Radian(raw) + m_offsets[id]) / m_multipliers[id];
+    return joint;
+}
+
+/*! @brief  Converts a vector of joint angles in radians within the NUbots joint space into a vecor of raw Robotis MX28 motor values.
+
+  The NUbots joint space is the standard joint angle definitions constant between all robots. In order to
+  convert the angles into this space an offset and multiplier are applied to each angle.
+
+  @param body The vector of joint angle to be converted.
+  @return The vector of raw motor values with the offset and multipler values removed, corresponding to the joint angles given as a parameter.
+ */
+std::vector<int> DarwinJointMapping::body2raw(const std::vector<float>& body) const
+{
+    std::vector<int> raw(body.size(), 0);
+    for(unsigned int id = 0; id < body.size(); ++id)
     {
-        joints[id] = convertJointPosition(id, joints[id]);
+        raw[id] = joint2raw(id, body[id]);
     }
-    return joints;
+    return raw;
+}
+
+/*! @brief  Converts a vector of Robotis MX28 motor values into a vecor of joint angles in radians within the NUbots joint space.
+
+  @param raw The raw Robotis MX28 motor value.
+  @return The vector of joint angles with the offset and multipler values applied, corresponding to the motor values given as a parameter.
+ */
+std::vector<float> DarwinJointMapping::raw2body(const std::vector<int>& raw) const
+{
+    std::vector<float> body(raw.size(), 0);
+    for(unsigned int id = 0; id < raw.size(); ++id)
+    {
+        body[id] = raw2joint(id, raw[id]);
+    }
+    return body;
 }
--- NUPlatform/Platforms/Darwin/DarwinJointMapping.h
+++ NUPlatform/Platforms/Darwin/DarwinJointMapping.h
@@ -1,3 +1,45 @@
+/*! @file DarwinJointMapping.h
+    @brief Declaration of DarwinJointMapping class
+
+    @class DarwinJointMapping
+    @brief A singleton class to convert between the raw data read from the Darwin robot motors,
+    and the joint angles used in the NUbots robotics software system.
+
+    The NUbots joint space is the standard joint angle definitions constant between all robots. The
+    zero position and direction of the motor angle is entierly dependant on the mounting of the motor.
+    Because of this we wish to convert the angles to a common frame of reference, in this case centred on the
+    origin coordinate system used for all other calculations. The right hand rule is applied so that a positive
+    angle is clockwise looking in the direction of each axis. Roll is about the x-axis that is directed forward
+    from the chest of the robot. Pitch is about the y-axis directed sideward to the left of the robot. Yaw is
+    about the z-axis directed upward through the head of the robot.
+    In order to convert the angles into this space an offset and multiplier are applied to each angle.
+
+    The static functions Value2Radian() and Radian2Value() are a direct conversion between the raw positive integer
+    value read from the Robotis MX28 motors and a motor angle in radians.
+
+    The functions joint2raw() and raw2joint() perform the conversion between joint angle and motor position value,
+    but also apply the offset and multiplier required to convert the motor angle into a standardised joint angle
+    within the Nubots software. The remaning body2raw() and raw2body() functions are used to perform this function
+    on the entire set of joint angles as opposed to a single joint.
+
+    @author Steven Nicklin
+
+    Copyright (c) 2011 Steven Nicklin
+
+    This file is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This file is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with NUbot.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef DARWINJOINTMAPPING_H
 #define DARWINJOINTMAPPING_H
 
@@ -6,10 +48,27 @@
 class DarwinJointMapping
 {
 public:
-    DarwinJointMapping();
-    static float convertJointPosition(unsigned int id, float position);
-    static std::vector<float> convertJointPositionValues(std::vector<float> joints);
-    std::vector<float>m_servo_offsets;
+    /*! @brief  Retrieve the single persistant instance of the DarwinJointMapping singleton class.
+        @return The single persistant instance of this class.
+     */
+    static DarwinJointMapping& Instance()
+    {
+      static DarwinJointMapping singleton;
+      return singleton;
+    }
+
+    int joint2raw(unsigned int id, float joint) const;
+    float raw2joint(unsigned int id, int raw) const;
+    std::vector<int> body2raw(const std::vector<float>& body) const;
+    std::vector<float> raw2body(const std::vector<int>& raw) const;
+    static int Radian2Value(float radian);
+    static float Value2Radian(int value);
+protected:
+    std::vector<float> m_offsets;                               //!< Table of offset values for each joint.
+    std::vector<char> m_multipliers;                            //!< Table of multiplier values for each joint.
+    DarwinJointMapping();                                       //!< Private constructor
+    DarwinJointMapping(const DarwinJointMapping&);              //!< Prevent copy-construction
+    DarwinJointMapping& operator=(const DarwinJointMapping&);   //!< Prevent assignment
 };
 
 #endif // DARWINJOINTMAPPING_H
