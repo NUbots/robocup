35ccc552cd7cdf89f4c7fad7722907d7bc6f3fe4
Josiah Walker <merlz@Thor.local>
2012-03-22 00:01:11 +1100

    Test Keeper Commit.

---
+++ Behaviour/GoalKeeperTest/TestKeeperProvider.h
@@ -0,0 +1,47 @@
+/*! @file GoalKeeperProvider.h
+    @brief Declaration of a behaviour provider for testing the goal keeper behaviour
+ 
+    @class GoalKeeperProvider
+    @brief A special behaviour for developing the goal keeper
+ 
+
+    @author Jason Kulk
+ 
+  Copyright (c) 2011 Jason Kulk
+ 
+    This file is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This file is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with NUbot.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef TESTKEEPER_PROVIDER_H
+#define TESTKEEPER_PROVIDER_H
+
+#include "../BehaviourProvider.h"
+
+class TestKeeperProvider : public BehaviourProvider
+{
+public:
+    TestKeeperProvider(Behaviour* manager);
+    ~TestKeeperProvider();
+protected:
+    void doBehaviour();
+
+private:
+    
+    float m_block_time;
+    bool doSave(float maxInterceptTime=18,float interceptErrorFraction=0.5);
+};
+
+
+#endif
+
