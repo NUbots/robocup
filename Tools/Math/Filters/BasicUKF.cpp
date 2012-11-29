4d8cccbac581308173d53c08cff9fd770e49e0b9
Steven Nicklin <steven.nicklin@gmail.com>
2012-11-29 15:39:06 +1100

    Added basic UKF implementation.

---
+++ Tools/Math/Filters/BasicUKF.cpp
@@ -0,0 +1,260 @@
+#include "BasicUKF.h"
+#include "Tools/Math/General.h"
+#include "IKFModel.h"
+#include <sstream>
+
+BasicUKF::BasicUKF(IKFModel *model): IKalmanFilter(model), m_estimate(model->totalStates()), m_unscented_transform(model->totalStates())
+{
+    init();
+}
+
+BasicUKF::BasicUKF(const BasicUKF& source): IKalmanFilter(source.m_model), m_estimate(source.m_estimate), m_unscented_transform(source.m_unscented_transform)
+{
+    init();
+    m_filter_weight = source.m_filter_weight;
+}
+
+BasicUKF::~BasicUKF()
+{
+}
+
+void BasicUKF::init()
+{
+    CalculateWeights();
+    initialiseEstimate(m_estimate);
+    m_outlier_filtering_enabled = false;
+    m_weighting_enabled = false;
+    m_outlier_threshold = 15.f;
+    m_filter_weight = 1.f;
+}
+
+/*!
+ * @brief Pre-calculate the sigma point weightings, based on the current unscented transform parameters.
+ */
+void BasicUKF::CalculateWeights()
+{
+    const unsigned int totalWeights = m_unscented_transform.totalSigmaPoints();
+
+    // initialise to correct sizes. These are row vectors.
+    m_mean_weights = Matrix(1, totalWeights, false);
+    m_covariance_weights = Matrix(1, totalWeights, false);
+
+    // Calculate the weights.
+    for (unsigned int i = 0; i < totalWeights; ++i)
+    {
+        m_mean_weights[0][i] = m_unscented_transform.Wm(i);
+        m_covariance_weights[0][i] = m_unscented_transform.Wc(i);
+    }
+    return;
+}
+
+/*!
+ * @brief Calculates the sigma points from the current mean an covariance of the filter.
+ * @return The sigma points that describe the current mean and covariance.
+ */
+Matrix BasicUKF::GenerateSigmaPoints() const
+{
+    const unsigned int numPoints = m_unscented_transform.totalSigmaPoints();
+    const Matrix current_mean = m_estimate.mean();
+    const unsigned int num_states = m_estimate.totalStates();
+    Matrix points(num_states, numPoints, false);
+
+    points.setCol(0, current_mean); // First sigma point is the current mean with no deviation
+    Matrix sqtCovariance = cholesky(m_unscented_transform.covarianceSigmaWeight() * m_estimate.covariance());
+    Matrix deviation;
+
+    for(unsigned int i = 1; i < num_states + 1; i++){
+        int negIndex = i+num_states;
+        deviation = sqtCovariance.getCol(i - 1);        // Get deviation from weighted covariance
+        points.setCol(i, (current_mean + deviation));         // Add mean + deviation
+        points.setCol(negIndex, (current_mean - deviation));  // Add mean - deviation
+    }
+    return points;
+}
+
+/*!
+ * @brief Calculate the mean from a set of sigma points.
+ * @param sigmaPoints The sigma points.
+ * @return The mean of the given sigma points.
+ */
+Matrix BasicUKF::CalculateMeanFromSigmas(const Matrix& sigmaPoints) const
+{
+    return sigmaPoints * m_mean_weights.transp();
+}
+
+/*!
+ * @brief Calculate the mean from a set of sigma points, given the mean of these points.
+ * @param sigmaPoints The sigma points.
+ * @param mean The mean of the sigma points.
+ * @return The covariance of the given sigma points.
+ */
+Matrix BasicUKF::CalculateCovarianceFromSigmas(const Matrix& sigmaPoints, const Matrix& mean) const
+{
+    const unsigned int numPoints = m_unscented_transform.totalSigmaPoints();
+    const unsigned int numStates = m_estimate.totalStates();
+    Matrix covariance(numStates, numStates, false);  // Blank covariance matrix.
+    Matrix diff;
+    for(unsigned int i = 0; i < numPoints; ++i)
+    {
+        const double weight = m_covariance_weights[0][i];
+        diff = sigmaPoints.getCol(i) - mean;
+        covariance = covariance + weight*diff*diff.transp();
+    }
+    return covariance;
+}
+
+
+/*!
+ * @brief Performs the time update of the filter.
+ * @param deltaT The time that has passed since the previous update.
+ * @param measurement The measurement/s (if any) that can be used to measure a change in the system.
+ * @param linearProcessNoise The linear process noise that will be added.
+ * @return True if the time update was performed successfully. False if it was not.
+ */
+bool BasicUKF::timeUpdate(double delta_t, const Matrix& measurement, const Matrix& process_noise, const Matrix& measurement_noise)
+{
+    const unsigned int total_points = m_unscented_transform.totalSigmaPoints();
+
+    // Calculate the current sigma points, and write to member variable.
+    Matrix sigma_points = GenerateSigmaPoints();
+    Matrix current_point; // temporary storage.
+
+    // update each sigma point.
+    for (unsigned int i = 0; i < total_points; ++i)
+    {
+        current_point = sigma_points.getCol(i);    // Get the sigma point.
+        sigma_points.setCol(i, m_model->processEquation(current_point, delta_t, measurement));   // Write the propagated version of it.
+    }
+
+    // Calculate the new mean and covariance values.
+    Matrix predicted_mean = CalculateMeanFromSigmas(sigma_points);
+    Matrix predicted_covariance = CalculateCovarianceFromSigmas(sigma_points, predicted_mean) + process_noise;
+
+    // Set the new mean and covariance values.
+    initialiseEstimate(Moment(predicted_mean, predicted_covariance));
+    return false;
+}
+
+/*!
+ * @brief Performs the measurement update of the filter.
+ * @param measurement The measurement to be used for the update.
+ * @param measurementNoise The linear measurement noise that will be added.
+ * @param measurementArgs Any additional information about the measurement, if required.
+ * @return True if the measurement update was performed successfully. False if it was not.
+ */
+bool BasicUKF::measurementUpdate(const Matrix& measurement, const Matrix& noise, const Matrix& args, unsigned int type)
+{
+    const unsigned int total_points = m_unscented_transform.totalSigmaPoints();
+    const unsigned int numStates = m_model->totalStates();
+    const unsigned int totalMeasurements = measurement.getm();
+
+    Matrix sigma_points = GenerateSigmaPoints();
+    Matrix sigma_mean = m_estimate.mean();
+
+    Matrix current_point; // temporary storage.
+    Matrix Yprop(totalMeasurements, total_points);
+
+    // First step is to calculate the expected measurmenent for each sigma point.
+    for (unsigned int i = 0; i < total_points; ++i)
+    {
+        current_point = sigma_points.getCol(i);    // Get the sigma point.
+        Yprop.setCol(i, m_model->measurementEquation(current_point, args, type));
+    }
+
+    // Now calculate the mean of these measurement sigmas.
+    Matrix Ymean = CalculateMeanFromSigmas(Yprop);
+
+    Matrix Pyy(noise);   // measurement noise is added, so just use as the beginning value of the sum.
+    Matrix Pxy(numStates, totalMeasurements, false); // initialised as 0
+
+    // Calculate the Pyy and Pxy variance matrices.
+    for(unsigned int i = 0; i < total_points; ++i)
+    {
+        double weight = m_covariance_weights[0][i];
+        // store difference between prediction and measurement.
+        current_point = Yprop.getCol(i) - Ymean;
+        // Innovation covariance - Add Measurement noise
+        Pyy = Pyy + weight * current_point * current_point.transp();
+        // Cross correlation matrix
+        Pxy = Pxy + weight * (sigma_points.getCol(i) - sigma_mean) * current_point.transp();    // Important: Use mean from estimate, not current mean.
+    }
+
+    const Matrix innovation = measurement - Ymean;
+
+    // Check for outlier, if outlier return without updating estimate.
+    if(evaluateMeasurement(innovation, Pyy, noise) == false) // Y * Y^T is the estimate variance, by definition.
+        return false;
+
+    // Calculate the Kalman filter gain
+    Matrix K = Pxy * InverseMatrix(Pyy);
+
+    Matrix new_mean = m_estimate.mean() + K * innovation;
+    Matrix new_covariance = m_estimate.covariance() - K*Pyy*K.transp();
+
+    initialiseEstimate(Moment(new_mean, new_covariance));
+    return true;
+}
+
+void BasicUKF::initialiseEstimate(const Moment& estimate)
+{
+    // This is pretty simple.
+    // Assign the estimate.
+    m_estimate = estimate;
+
+    return;
+}
+
+const Moment& BasicUKF::estimate() const
+{
+    return m_estimate;
+}
+
+bool BasicUKF::evaluateMeasurement(const Matrix& innovation, const Matrix& estimate_variance, const Matrix& measurement_variance)
+{
+    if(!m_outlier_filtering_enabled and !m_weighting_enabled) return true;
+
+    const Matrix innov_transp = innovation.transp();
+    const Matrix sum_variance = estimate_variance + measurement_variance;
+
+    if(m_outlier_filtering_enabled)
+    {
+        float innovation_2 = convDble(innov_transp * InverseMatrix(sum_variance) * innovation);
+        if(m_outlier_threshold > 0 and innovation_2 > m_outlier_threshold)
+            return false;
+    }
+
+    if(m_weighting_enabled)
+    {
+        m_filter_weight *= 1 / ( 1 + convDble(innov_transp * InverseMatrix(measurement_variance) * innovation));
+    }
+
+    return true;
+}
+
+std::string BasicUKF::summary(bool detailed) const
+{
+    std::stringstream str_strm;
+    str_strm << "ID: " << m_id <<  " Weight: " << m_filter_weight << std::endl;
+    str_strm << m_estimate.string() << std::endl;
+    return str_strm.str();
+}
+
+std::ostream& BasicUKF::writeStreamBinary (std::ostream& output) const
+{
+    m_model->writeStreamBinary(output);
+    m_unscented_transform.writeStreamBinary(output);
+    m_estimate.writeStreamBinary(output);
+    return output;
+}
+
+std::istream& BasicUKF::readStreamBinary (std::istream& input)
+{
+    m_model->readStreamBinary(input);
+    m_unscented_transform.readStreamBinary(input);
+    Moment temp;
+    temp.readStreamBinary(input);
+    CalculateWeights();
+    initialiseEstimate(temp);
+    return input;
+}
+
---
+++ Tools/Math/Filters/BasicUKF.h
@@ -0,0 +1,100 @@
+#ifndef BASICUKF_H
+#define BASICUKF_H
+
+#include "IKalmanFilter.h"
+#include "Tools/Math/Matrix.h"
+#include "Tools/Math/Moment.h"
+#include "UnscentedTransform.h"
+
+class BasicUKF: public IKalmanFilter
+{
+public:
+    BasicUKF(IKFModel* model);
+    BasicUKF(const BasicUKF& source);
+    ~BasicUKF();
+
+    /*!
+    @brief Time update function
+    The time update function predicts the new state of the system.
+    @param delta_t The elapsed time since the previous time update.
+    @param measurement Any mesurements that can be used to predict the change in state.
+    @param process_noise The linear process noise to be added to the estimate.
+    @param measurement_noise The noise present in the measurment provided.
+    @return True if the update was performed sucessfully. False if the update was unable to be performed.
+    */
+    virtual bool timeUpdate(double delta_t, const Matrix& measurement, const Matrix& process_noise, const Matrix& measurement_noise);
+
+    /*!
+    @brief Measurement update function
+    The measurement update function corrects the estimated state of the system using observed measurement/s.
+    @param measurement The measured data to be used for the update.
+    @param noise The noise associated with the measurement data provided.
+    @param args Any additional arguments required for the measurment update.
+    @param type The type of measurement.
+    @return True if the update was performed sucessfully. False if the update was unable to be performed.
+    */
+    virtual bool measurementUpdate(const Matrix& measurement, const Matrix& noise, const Matrix& args, unsigned int type);
+
+    /*!
+    @brief Initialisation function.
+    Used to initialise the filters estimate.
+    @param estimate The initial estimate of the filter.
+    */
+    void initialiseEstimate(const Moment& estimate);
+
+    /*!
+    @brief Get function for the estimate.
+    Retrieves the filters current best estimate for the system.
+    @return The current estimate of the filter.
+    */
+    const Moment& estimate() const;
+
+    std::string summary(bool detailed) const;
+
+    /*!
+    @brief Outputs a binary representation of the UKF object to a stream.
+    @param output The output stream.
+    @return The output stream.
+    */
+    std::ostream& writeStreamBinary (std::ostream& output) const;
+
+    /*!
+    @brief Reads in a UKF object from the input stream.
+    @param input The input stream.
+    @return The input stream.
+    */
+    std::istream& readStreamBinary (std::istream& input);
+
+    // Weighting functions.
+    void enableWeighting(bool enabled = true) {m_weighting_enabled = enabled;}
+    float getFilterWeight() const {return m_filter_weight;}
+    void setFilterWeight(float weight) {m_filter_weight = weight;}
+
+    // Outlier filtering settings.
+    void enableOutlierFiltering(bool enabled = true) {m_outlier_filtering_enabled = enabled;}
+    void setOutlierThreshold(float new_threshold){m_outlier_threshold = new_threshold;}
+    bool outlierFiltering() const {return m_outlier_filtering_enabled;}
+    float outlierThreshold() const {return m_outlier_threshold;}
+
+private:
+    bool m_outlier_filtering_enabled;
+    float m_outlier_threshold;
+    bool m_weighting_enabled;
+    float m_filter_weight;
+    Moment m_estimate;
+    Matrix m_mean_weights;
+    Matrix m_covariance_weights;
+
+    UnscentedTransform m_unscented_transform;
+
+    void init();
+    bool evaluateMeasurement(const Matrix& innovation, const Matrix& estimate_variance, const Matrix& measurement_variance);
+
+    // Functions for performing steps of the UKF algorithm.
+    void CalculateWeights();
+    Matrix GenerateSigmaPoints() const;
+    Matrix CalculateMeanFromSigmas(const Matrix& sigmaPoints) const;
+    Matrix CalculateCovarianceFromSigmas(const Matrix& sigmaPoints, const Matrix& mean) const;
+};
+
+#endif // BASICUKF_H
