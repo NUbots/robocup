378ad707277ab47d61f22cf13fd93495f37baa4e
Steven Nicklin <steven.nicklin@gmail.com>
2013-03-01 19:37:07 +1100

    Removed BatchSelectionDialog code.

--- NUView/offlinelocalisationdialog.h
+++ NUView/offlinelocalisationdialog.h
@@ -62,31 +62,4 @@ private:
 };
 
 
-class BatchSelectDialog : public QDialog
-{
-    Q_OBJECT
-public slots:
-    void selectSourcePath();
-    void selectResultPath();
-    void verifySourcePath(QString text);
-    void verifyResultPath(QString text);
-
-public:
-    explicit BatchSelectDialog(QWidget *parent = 0);
-    ~BatchSelectDialog();
-    QString sourcePath();
-    QString resultPath();
-    QString experimentType();
-
-private:
-    bool verifyAndDisplay(QString text, QLabel* label);
-    void makeLayout();
-    QComboBox* m_experiment_select;
-    QLineEdit* m_source_path_edit;
-    QLineEdit* m_result_path_edit;
-    QLabel* m_source_path_verified;
-    QLabel* m_result_path_verified;
-private:
-};
-
 #endif // OFFLINELOCALISATIONDIALOG_H
