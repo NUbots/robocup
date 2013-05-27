ee0063979e46e580ebd9bddc9788537d6fe16ee0
Steven Nicklin <steven.nicklin@gmail.com>
2013-05-27 14:52:44 +1000

    Completed some more of the class.

--- NUView/SensorCalibrationWidget.cpp
+++ NUView/SensorCalibrationWidget.cpp
@@ -5,6 +5,11 @@
 #include "Tools/Math/General.h"
 #include <QDebug>
 #include <iostream>
+#include "Vision/VisionTools/transformer.h"
+#include "Kinematics/Kinematics.h"
+#include "Tools/Math/TransformMatrices.h"
+#include <QFileDialog>
+#include <QDir>
 
 SensorCalibrationWidget::SensorCalibrationWidget(QWidget *parent) :
     QWidget(parent),
@@ -21,241 +26,155 @@ SensorCalibrationWidget::SensorCalibrationWidget(QWidget *parent) :
     connect(ui->doubleSpinBox_camera_roll,SIGNAL(valueChanged(double)), this, SLOT(ValueChanged()));
     connect(ui->doubleSpinBox_camera_pitch,SIGNAL(valueChanged(double)), this, SLOT(ValueChanged()));
     connect(ui->doubleSpinBox_camera_yaw,SIGNAL(valueChanged(double)), this, SLOT(ValueChanged()));
+    connect(ui->comboBox_camera_select,SIGNAL(currentIndexChanged(int)), this, SLOT(ValueChanged()));
     m_sensors = NULL;
     selected_x = -1;
     selected_y = -1;
+
+    m_transformer = new Transformer();
+    m_transformer->setCamParams(Vector2<double>(640,480), Vector2<double>(1.0472, 0.7854));
+    m_transformer->setCalibration(m_calibration.m_calibration);
+
+    m_calibration.m_calibration.m_neck_position_offset = Vector3<double>(0.0, 0.0, 5.05);
+
+    m_updating = false;
 }
 
 SensorCalibrationWidget::~SensorCalibrationWidget()
 {
+    if(m_transformer) delete m_transformer;
     delete ui;
 }
 
-SensorCalibration* SensorCalibrationWidget::Calibration()
+SensorCalibrationSettings* SensorCalibrationWidget::Calibration()
 {
     return &m_calibration;
 }
 
 void SensorCalibrationWidget::ValueChanged()
 {
+    if(m_updating) return;
+
     // Location
     m_calibration.location_x = ui->doubleSpinBox_field_x->value();
     m_calibration.location_y = ui->doubleSpinBox_field_y->value();
     m_calibration.location_orientation = ui->doubleSpinBox_field_ori->value();
 
     // Body offsets.
-    m_calibration.body_roll_offset = ui->doubleSpinBox_body_roll->value();
-    m_calibration.body_pitch_offset = ui->doubleSpinBox_body_pitch->value();
+    m_calibration.m_calibration.m_body_angle_offset.x = ui->doubleSpinBox_body_roll->value();
+    m_calibration.m_calibration.m_body_angle_offset.y = ui->doubleSpinBox_body_pitch->value();
 
     // Camera offsets.
-    m_calibration.camera_roll_offset = ui->doubleSpinBox_camera_roll->value();
-    m_calibration.camera_pitch_offset = ui->doubleSpinBox_camera_pitch->value();
-    m_calibration.camera_yaw_offset = ui->doubleSpinBox_camera_yaw->value();
+    m_calibration.m_calibration.m_camera_angle_offset.x = ui->doubleSpinBox_camera_roll->value();
+    m_calibration.m_calibration.m_camera_angle_offset.y = ui->doubleSpinBox_camera_pitch->value();
+    m_calibration.m_calibration.m_camera_angle_offset.z = ui->doubleSpinBox_camera_yaw->value();
+
+    if(ui->comboBox_camera_select->currentText().toLower() == "darwin")
+    {
+        // From CameraSpecs.cfg file.
+        m_calibration.m_fov.x = 60.f;
+        m_calibration.m_fov.y = 46.f;
+        Vector2<double> rad_fov(mathGeneral::deg2rad(m_calibration.m_fov.x), mathGeneral::deg2rad(m_calibration.m_fov.y));
+        m_transformer->setCamParams(Vector2<double>(640,480), rad_fov);
+    }
+    else if(ui->comboBox_camera_select->currentText().toLower() == "darwinwebots")
+    {
+        // From webots model
+        m_calibration.m_fov.x = 60.f;
+        m_calibration.m_fov.y = 45.f;
+        Vector2<double> rad_fov(mathGeneral::deg2rad(m_calibration.m_fov.x), mathGeneral::deg2rad(m_calibration.m_fov.y));
+        m_transformer->setCamParams(Vector2<double>(640,480), rad_fov);
+    }
+    else
+    {
+        // default
+        m_calibration.m_fov.x = 60.f;
+        m_calibration.m_fov.y = 45.f;
+        Vector2<double> rad_fov(mathGeneral::deg2rad(m_calibration.m_fov.x), mathGeneral::deg2rad(m_calibration.m_fov.y));
+        m_transformer->setCamParams(Vector2<double>(640,480), rad_fov);
+    }
 
     emit CalibrationChanged(&m_calibration);
 
     if(selected_x >= 0 and selected_y >= 0)
         select_pixel(selected_x, selected_y);
+
+    m_transformer->setCalibration(m_calibration.m_calibration);
 }
 
 void SensorCalibrationWidget::setSensorData(NUSensorsData* new_sensordata)
 {
     m_sensors = new_sensordata;
+
+    if(m_sensors)
+    {
+        float head_pitch(0.f), head_yaw(0.f);
+        Vector3<double> neck_position;
+        vector<float> orientation(3,0.f), left, right;
+        m_sensors->getPosition(NUSensorsData::HeadPitch, head_pitch);
+        m_sensors->getPosition(NUSensorsData::HeadYaw, head_yaw);
+        m_sensors->getOrientation(orientation);
+
+        if(m_sensors->get(NUSensorsData::LLegTransform, left) and m_sensors->get(NUSensorsData::RLegTransform, right))
+        {
+            neck_position = Kinematics::CalculateNeckPosition(Matrix4x4fromVector(left), Matrix4x4fromVector(right), m_calibration.m_calibration.m_neck_position_offset);
+        }
+        else
+        {
+            // Default in case kinemtaics not available. Base height of darwin.
+            neck_position = Vector3<double>(0.0, 0.0, 39.22);
+        }
+        m_transformer->setSensors(head_pitch, head_yaw, orientation.at(0), orientation.at(1), neck_position);
+    }
 }
 
 void SensorCalibrationWidget::select_pixel(int x, int y)
 {
-    if(m_sensors)
-    {
-        const unsigned int image_width = 640;
-        const unsigned int image_height = 480;
-        const unsigned int diagnal_dist = sqrt(image_width*image_width + image_height*image_height);
+    Vector3<double> result = m_transformer->distanceToPoint(x, y);
 
-//        const float fov_diag = 1.0472;
-//        const float f = 0.5f * diagnal_dist / tan(0.5f * fov_diag);
-//        const float fov_horizontal = 2 * atan(0.5f * image_width / f);
-//        const float fov_vertical = 2 * atan(0.5f * image_height / f);
+    double flatDist = result.x * cos(result.z);
+    double x_pos = flatDist * cos(result.y);
+    double y_pos = flatDist * sin(result.y);
+    double z_pos = result.x * sin(result.z);
 
-        const float fov_horizontal = 1.0472;
-        const float fov_vertical = fov_horizontal * image_height / image_width;
-
-        const float f = 0.5f * image_width / tan(0.5f * fov_horizontal);
-
-        std::cout << "f: " << f << std::endl;
-        std::cout << "fov_horizontal: " << fov_horizontal << std::endl;
-        std::cout << "fov_vertical: " << fov_vertical << std::endl;
-
-        // Varibles for orientations.
-        float camera_roll = m_calibration.camera_roll_offset;
-        float camera_pitch = m_calibration.camera_pitch_offset;
-        float camera_yaw = m_calibration.camera_yaw_offset;
-
-        float head_roll = 0.f;
-        float head_pitch = 0.f;
-        float head_yaw = 0.f;
-
-        float body_roll = m_calibration.body_roll_offset;
-        float body_pitch = m_calibration.body_pitch_offset;
-        float body_yaw = 0.f;
-
-        // Camera offset values. (These should not be hard-coded since they differ per robot type)
-        const float camera_x_offset = 3.32f;
-        const float camera_y_offset = 0.f;
-        const float camera_z_offset = 3.44f;
-
-        // Body offset values. (These should not be hard-coded since they differ per robot type)
-        const float body_x_offset = 0.f;
-        const float body_y_offset = 0.f;
-        const float body_z_offset = 39.22f;
-
-        // Get the sensor information.
-        float temp = 0.f;   // temp variable for fetching sensor values.
-        std::vector<float> temp_vec;
-        if(m_sensors)  // Check if sensor data is available
-        {
-            // Head pitch
-            if(m_sensors->getPosition(NUSensorsData::HeadPitch, temp))
-            {
-                head_pitch += temp;
-            }
-            // Head yaw
-            if(m_sensors->getPosition(NUSensorsData::HeadYaw, temp))
-            {
-                head_yaw += temp;
-            }
-            // Head roll - Our robots do not have thus at the moment.
-            if(m_sensors->getPosition(NUSensorsData::HeadRoll, temp))
-            {
-                head_roll += temp;
-            }
-    //        if(m_sensors->getOrientation(temp_vec))
-    //        {
-    //            body_roll += temp_vec[0];
-    //            body_pitch += temp_vec[1];
-    //            body_yaw += temp_vec[2];
-
-    //            std::cout << body_roll << ", " << body_pitch << ", " << body_yaw << std::endl;
-    //        }
-            // Camera Height
-    //        if(SensorData->getCameraHeight(temp))
-    //        {
-    //            std::cout << "Camera Height: " << temp << std::endl;
-    //        }
-        }
-
-        ///
-        // Testing area
-        ///
-        Matrix camOffsetVec(3,1,false);
-        camOffsetVec[0][0] = camera_x_offset;
-        camOffsetVec[1][0] = camera_y_offset;
-        camOffsetVec[2][0] = camera_z_offset;
-
-        std::cout << "camOffsetVec" << std::endl << camOffsetVec << std::endl;
-
-        Matrix neckV(3,1,false);
-        neckV[0][0] = body_x_offset;
-        neckV[1][0] = body_y_offset;
-        neckV[2][0] = body_z_offset;
-
-        std::cout << "neckV" << std::endl << neckV << std::endl;
-
-        double sinA;
-        double cosA;
-
-        Matrix body_roll_rot = Matrix(3,3,true);
-        sinA = sin(body_roll);
-        cosA = cos(body_roll);
-        body_roll_rot[1][1] = cosA;
-        body_roll_rot[1][2] = -sinA;
-        body_roll_rot[2][1] = sinA;
-        body_roll_rot[2][2] = cosA;
-
-        Matrix camera_roll_rot = Matrix(3,3,true);
-        sinA = sin(camera_roll);
-        cosA = cos(camera_roll);
-        camera_roll_rot[1][1] = cosA;
-        camera_roll_rot[1][2] = -sinA;
-        camera_roll_rot[2][1] = sinA;
-        camera_roll_rot[2][2] = cosA;
-
-        Matrix body_pitch_rot = Matrix(3,3,true);
-        sinA = sin(body_pitch);
-        cosA = cos(body_pitch);
-        body_pitch_rot[0][0] = cosA;
-        body_pitch_rot[0][2] = sinA;
-        body_pitch_rot[2][0] = -sinA;
-        body_pitch_rot[2][2] = cosA;
-
-        Matrix camera_pitch_rot = Matrix(3,3,true);
-        sinA = sin(camera_pitch);
-        cosA = cos(camera_pitch);
-        camera_pitch_rot[0][0] = cosA;
-        camera_pitch_rot[0][2] = sinA;
-        camera_pitch_rot[2][0] = -sinA;
-        camera_pitch_rot[2][2] = cosA;
-
-        Matrix head_pitch_rot = Matrix(3,3,true);
-        sinA = sin(head_pitch);
-        cosA = cos(head_pitch);
-        head_pitch_rot[0][0] = cosA;
-        head_pitch_rot[0][2] = sinA;
-        head_pitch_rot[2][0] = -sinA;
-        head_pitch_rot[2][2] = cosA;
-
-        Matrix head_yaw_rot = Matrix(3,3,true);
-        sinA = sin(head_yaw);
-        cosA = cos(head_yaw);
-        head_yaw_rot[0][0] = cosA;
-        head_yaw_rot[0][1] = -sinA;
-        head_yaw_rot[1][0] = sinA;
-        head_yaw_rot[1][1] = cosA;
-
-        Matrix camera_yaw_rot = Matrix(3,3,true);
-        sinA = sin(camera_yaw);
-        cosA = cos(camera_yaw);
-        camera_yaw_rot[0][0] = cosA;
-        camera_yaw_rot[0][1] = -sinA;
-        camera_yaw_rot[1][0] = sinA;
-        camera_yaw_rot[1][1] = cosA;
-
-        Matrix headV2RobotRotation = body_roll_rot * body_pitch_rot * head_yaw_rot * head_pitch_rot;
-        std::cout << "headV2RobotRotation" << std::endl << headV2RobotRotation << std::endl;
-
-        Matrix camVector = (headV2RobotRotation * camOffsetVec) + neckV;
-        std::cout << "camVector" << std::endl << camVector << std::endl;
-        Matrix camV2RobotRotation = headV2RobotRotation * camera_pitch_rot * camera_roll_rot * camera_yaw_rot;
-        std::cout << "camV2RobotRotation" << std::endl << camV2RobotRotation << std::endl;
-
-        int cx = x;
-        int cy = y;
-        float objectHeight = 0;
-
-        Matrix vcam(3,1,false);
-        vcam[0][0] = f;
-        vcam[1][0] = image_width / 2.f - cx;
-        vcam[2][0] = image_height / 2.f - cy;
-        std::cout << "vcam" << std::endl << vcam << std::endl;
-
-        Matrix roboVdir = camV2RobotRotation * vcam;
-        std::cout << "roboVdir" << std::endl << roboVdir << std::endl;
-        double alpha = (objectHeight - camVector[2][0]) / roboVdir[2][0];
-        std::cout << "alpha" << std::endl << alpha << std::endl;
-
-        Matrix v2FieldPoint = alpha * roboVdir + camVector;
-
-        std::cout << "v2FieldPoint" << std::endl << v2FieldPoint << std::endl;
-
-        double distance = sqrt(pow(v2FieldPoint[0][0], 2) + pow(v2FieldPoint[1][0], 2));
-        double bearing = atan2((double)v2FieldPoint[1][0], (double)v2FieldPoint[0][0]);
-        double elevation = atan2(objectHeight - (double)camVector[2][0], (double)distance);
-
-        std::cout << "Pixel: (" << x << ", " << y << ")" << std::endl;
-
-        std::cout << "Distance: " << distance << std::endl;
-        std::cout << "Bearing: " << bearing << std::endl;
-        std::cout << "Elevation: " << elevation << std::endl;
-    }
-
+    QString message = "Pixel: (%1, %2)\nSpherical Position: (%3, %4, %5)\nCart Position: (%6, %7, %8)";
+    ui->textOutput->setText(message.arg(x).arg(y).arg(result.x,0,'g',3).arg(result.y,0,'g',3).arg(result.z,0,'g',3).arg(x_pos,0,'g',3).arg(y_pos,0,'g',3).arg(z_pos,0,'g',3));
 }
 
+
+void SensorCalibrationWidget::on_loadButton_clicked()
+{
+    QString init_path = QDir::homePath() + QDir::separator() + "*";
+    QString filename = QFileDialog::getOpenFileName(this, "Open configuration file", init_path, "Configuration files (*.cfg);;All Files(*.*)");
+
+    if(!filename.isEmpty())
+    {
+        m_updating = true;  // Flag to stop reading of changed values.
+
+        SensorCalibration calibration(filename.toStdString());
+
+        // Body offsets.
+        ui->doubleSpinBox_body_roll->setValue(calibration.m_body_angle_offset.x);
+        ui->doubleSpinBox_body_pitch->setValue(calibration.m_body_angle_offset.y);
+
+        // Camera offsets.
+        ui->doubleSpinBox_camera_roll->setValue(calibration.m_camera_angle_offset.x);
+        ui->doubleSpinBox_camera_pitch->setValue(calibration.m_camera_angle_offset.y);
+        ui->doubleSpinBox_camera_yaw->setValue(calibration.m_camera_angle_offset.z);
+
+        m_calibration.m_calibration = calibration;
+
+        m_updating = false;
+        update();
+    }
+}
+
+void SensorCalibrationWidget::on_saveButton_clicked()
+{
+    QString init_path = QDir::homePath() + QDir::separator() + "SensorCalibration.cfg";
+    QString filename = QFileDialog::getSaveFileName(this, "Open configuration file", init_path, "Configuration files (*.cfg);;All Files(*.*)");
+    if(!filename.isEmpty())
+    {
+        m_calibration.m_calibration.WriteSettings(filename.toStdString());
+    }
+}
--- NUView/SensorCalibrationWidget.h
+++ NUView/SensorCalibrationWidget.h
@@ -7,24 +7,20 @@ namespace Ui {
 class SensorCalibrationWidget;
 }
 
-class SensorCalibration
+#include "Infrastructure/SensorCalibration.h"
+class Transformer;
+class SensorCalibrationSettings
 {
 public:
     // Location
-    float location_x;
-    float location_y;
-    float location_orientation;
+    float location_x;               //! X position on the field.
+    float location_y;               //! Y position on the field.
+    float location_orientation;     //! Orientation on the field.
 
-    // Body offsets.
-    float body_roll_offset;
-    float body_pitch_offset;
+    Vector2<double> m_fov;               //! Field of view of the current camera (hard coded options at the moment.)
+    SensorCalibration m_calibration;    //! Sensor calibration
 
-    // Camera Offsets
-    float camera_roll_offset;
-    float camera_pitch_offset;
-    float camera_yaw_offset;
-    SensorCalibration(): location_x(0.f), location_y(0.f), location_orientation(0.f), body_roll_offset(0.f), body_pitch_offset(0.f),
-        camera_roll_offset(0.f), camera_pitch_offset(0.f), camera_yaw_offset(0.f){}
+    SensorCalibrationSettings(): location_x(0.f), location_y(0.f), location_orientation(0.f){}
 };
 
 class SensorCalibrationWidget : public QWidget
@@ -33,23 +29,65 @@ class SensorCalibrationWidget : public QWidget
     
 public:
     explicit SensorCalibrationWidget(QWidget *parent = 0);
-    SensorCalibration* Calibration();
-    void setCalibration(SensorCalibration& calibration){m_calibration = calibration;}
     ~SensorCalibrationWidget();
 
+    /*!
+    * @brief Fetches the current calibration setting.
+    * @return The current calibration setting of the widget.
+    */
+    SensorCalibrationSettings* Calibration();
+
+    /*!
+    * @brief Sets the current calibration setting.
+    * @param calibration The new calibration setting of the widget.
+    */
+    void setCalibration(SensorCalibrationSettings& calibration){m_calibration = calibration;}
+
 signals:
-    void CalibrationChanged(SensorCalibration* newCalibration);
+    /*!
+    * @brief Signal indicating that the current calibration settings have changed.
+    * @param calibration The new calibration setting of the widget.
+    */
+    void CalibrationChanged(SensorCalibrationSettings* newCalibration);
 public slots:
+    /*!
+    * @brief Slot used to update the NUSensorData used by the widget.
+    * @param new_sensordata The new sensor data.
+    */
     void setSensorData(NUSensorsData* new_sensordata);
+    /*!
+    * @brief Slot used to select a pixel.
+    * @param x pixel x-position.
+    * @param y pixel y-position.
+    */
     void select_pixel(int x, int y);
 protected slots:
+    /*!
+    * @brief Slot used when one of the contols has been changed.
+    * The calibration is updated and the new calibration settings emitted.
+    */
     void ValueChanged();
+private slots:
+    /*!
+    * @brief Slot used when the load button is clicked.
+    * Opens a file select dialog and loads the chosen configuration file.
+    */
+    void on_loadButton_clicked();
+
+    /*!
+    * @brief Slot used when the save button is clicked.
+    * Opens a file select dialog and saves the current configuration to the selected file.
+    */
+    void on_saveButton_clicked();
+
 private:
     Ui::SensorCalibrationWidget *ui;
-    SensorCalibration m_calibration;
+    SensorCalibrationSettings m_calibration;
     NUSensorsData* m_sensors;
     int selected_x;
     int selected_y;
+    Transformer* m_transformer;
+    bool m_updating;
 
 };
 
