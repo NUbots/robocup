0e4fb990d2b9cdb42bc9383e779c4d34767ed8b5
jakefountain <jake.fountain@uon.edu.au>
2013-06-16 12:32:10 +1000

    User interface working. All that is left is implementing the methods.

--- Behaviour/ScriptTuner/ScriptTunerStates.cpp
+++ Behaviour/ScriptTuner/ScriptTunerStates.cpp
@@ -1,42 +1,46 @@
 #include "ScriptTunerStates.h"
+#include "ScriptTunerProvider.h"
+#include "Framework/darwin/Framework/include/JointData.h"
 
 using std::vector;
 using std::string;
 
 
 
-ScriptTunerState(ScriptTunerProvider* provider) : ScriptTunerSubState(provider) :string_id_to_int_id() {
-    string_id_to_int_id["RSP"] = (int)JointData::ID_R_SHOULDER_PITCH;
-    string_id_to_int_id["LSP"] = (int)JointData::ID_L_SHOULDER_PITCH;
-    string_id_to_int_id["RSR"] = (int)JointData::ID_R_SHOULDER_ROLL;
-    string_id_to_int_id["LSR"] = (int)JointData::ID_L_SHOULDER_ROLL;
-    string_id_to_int_id["RE"] = (int)JointData::ID_R_ELBOW;
-    string_id_to_int_id["LE"] = (int)JointData::ID_L_ELBOW;
-    string_id_to_int_id["RHY"] = (int)JointData::ID_R_HIP_YAW;
-    string_id_to_int_id["LHY"] = (int)JointData::ID_L_HIP_YAW;
-    string_id_to_int_id["RHR"] = (int)JointData::ID_R_HIP_ROLL;
-    string_id_to_int_id["LHR"] = (int)JointData::ID_L_HIP_ROLL;
-    string_id_to_int_id["RHP"] = (int)JointData::ID_R_HIP_PITCH;
-    string_id_to_int_id["LHP"] = (int)JointData::ID_L_HIP_PITCH;
-    string_id_to_int_id["RK"] = (int)JointData::ID_R_KNEE;
-    string_id_to_int_id["LK"] = (int)JointData::ID_L_KNEE;
-    string_id_to_int_id["RAP"] = (int)JointData::ID_R_ANKLE_PITCH;
-    string_id_to_int_id["LAP"] = (int)JointData::ID_L_ANKLE_PITCH;
-    string_id_to_int_id["RAR"] = (int)JointData::ID_R_ANKLE_ROLL;
-    string_id_to_int_id["LAR"] = (int)JointData::ID_L_ANKLE_ROLL;
-    string_id_to_int_id["HP"] = (int)JointData::ID_HEAD_PAN;
-    string_id_to_int_id["HT"] = (int)JointData::ID_HEAD_TILT;
+ScriptTunerState::ScriptTunerState(ScriptTunerProvider* provider) : ScriptTunerSubState(provider),string_id_to_int_id() {
+    string_id_to_int_id["RSP"] = (int)Robot::JointData::ID_R_SHOULDER_PITCH;
+    string_id_to_int_id["LSP"] = (int)Robot::JointData::ID_L_SHOULDER_PITCH;
+    string_id_to_int_id["RSR"] = (int)Robot::JointData::ID_R_SHOULDER_ROLL;
+    string_id_to_int_id["LSR"] = (int)Robot::JointData::ID_L_SHOULDER_ROLL;
+    string_id_to_int_id["RE"] = (int)Robot::JointData::ID_R_ELBOW;
+    string_id_to_int_id["LE"] = (int)Robot::JointData::ID_L_ELBOW;
+    string_id_to_int_id["RHY"] = (int)Robot::JointData::ID_R_HIP_YAW;
+    string_id_to_int_id["LHY"] = (int)Robot::JointData::ID_L_HIP_YAW;
+    string_id_to_int_id["RHR"] = (int)Robot::JointData::ID_R_HIP_ROLL;
+    string_id_to_int_id["LHR"] = (int)Robot::JointData::ID_L_HIP_ROLL;
+    string_id_to_int_id["RHP"] = (int)Robot::JointData::ID_R_HIP_PITCH;
+    string_id_to_int_id["LHP"] = (int)Robot::JointData::ID_L_HIP_PITCH;
+    string_id_to_int_id["RK"] = (int)Robot::JointData::ID_R_KNEE;
+    string_id_to_int_id["LK"] = (int)Robot::JointData::ID_L_KNEE;
+    string_id_to_int_id["RAP"] = (int)Robot::JointData::ID_R_ANKLE_PITCH;
+    string_id_to_int_id["LAP"] = (int)Robot::JointData::ID_L_ANKLE_PITCH;
+    string_id_to_int_id["RAR"] = (int)Robot::JointData::ID_R_ANKLE_ROLL;
+    string_id_to_int_id["LAR"] = (int)Robot::JointData::ID_L_ANKLE_ROLL;
+    string_id_to_int_id["HP"] = (int)Robot::JointData::ID_HEAD_PAN;
+    string_id_to_int_id["HT"] = (int)Robot::JointData::ID_HEAD_TILT;
+    std::cout<< "==================================================="<< std::endl;
+    std::cout<< "--------------Welcome to Script Tuner--------------"<< std::endl;
+    std::cout<< "==================================================="<< std::endl;
 }
 
 
 void ScriptTunerState::doState()
 {
-
-
+    
     std::cout<< "Load Script - Type File Name (Must be in ~/nubot/Config/Darwin/Motion/Scripts): "<< std::endl;
     char file[256];
     std::cin.getline(file,256);
-    string filename(file);
+    filename = (string)file;
 
     if((filename.compare("exit") ==0) or (filename.compare("quit") == 0) ){
         std::cout<< "Shutting down script tuner."<< std::endl;
@@ -47,16 +51,37 @@ void ScriptTunerState::doState()
 
     auto loaded = loadScript(filename);
 
+
     if(loaded){
-        while(script.isActive()){
-            applyCurrentFrameToRobot();
-            std::cout << "Script \""<< filename << "\" loaded successfully."<< std::endl;
-            std::cout<< "Frame number "<<script.getCurrentFrameNumber()<<" out of " << script.numberOfFrames()<<" applied."<<std::endl;
-            std::cout<<"Time period for frame to complete is "<< script.timeForCompletionOfCurrentFrame()<< " seconds."<<std::endl;
+        bool done = false;
+        while(!done){
+            std::cout<<"Script \""<< filename << "\" loaded successfully."<< std::endl;
+            std::cout<< "Play script or edit? (Type \"play\" or \"edit\")"<< std::endl;
+            std::stringstream command;
+            char str[256];
+            std::cin.getline(str, 256);
+            command << str;
+            string first_argument;
+            command >> first_argument;
 
-            editCurrentFrame();
+            if(first_argument.compare("play")==0){            
+                std::cout << "Playing script from beginning in real time."<< std::endl;
+                playScript();
+            }else if(first_argument.compare("edit")==0){
+                std::cout<<"Script \""<< filename << "\" loaded successfully for editing."<< std::endl;                    
+                
+                while(scriptIsActive()){
+                    applyCurrentFrameToRobot();
+                    std::cout<<"Frame number "<<getCurrentFrameNumber()<<" out of " << totalNumberOfFrames()<<" applied."<<std::endl;
+                    std::cout<<"Time period for frame to complete is "<< durationOfCurrentFrame()<< " seconds."<<std::endl;               
+                  
+                    editCurrentFrame();
+                }
+            } else if((first_argument.compare("exit") ==0) or (first_argument.compare("quit") == 0) or (first_argument.compare("q") == 0) ){
+                std::cout<< "Exiting Script..."<< std::endl;                
+                done = true;
+            }
         }
-
 
     } else {
         std::cout <<"File not found! Try again!" << std::endl;
@@ -76,32 +101,32 @@ void ScriptTunerState::editCurrentFrame(){
         command >> first_argument >> second_argument;
 
         if(first_argument.compare("saveframe")==0){
-            std::cout << "Saving frame. It is recommended all torques are on during saving."<< std::endl;
+            std::cout << "Saving manually adjusted motor positions. It is recommended all torques are on during saving."<< std::endl;
             saveManuallyMovedMotors();
         }else if(first_argument.compare("savescript")==0){
             std::cout << "Script \""<< filename << "\" saving."<< std::endl;
             saveScriptToFile(filename);
-        }else if(first_argument.compare("exit")==0){
+        }else if(first_argument.compare("exit")==0 or (first_argument.compare("quit") == 0) or (first_argument.compare("q") == 0) ){
             std::cout << "Exiting."<< std::endl;
             exitScript();
             break;
         }else if(first_argument.compare("next")==0){
             std::cout << "Moving to next frame."<< std::endl;
-            moveToFrame(script.getCurrentFrameNumber()+1);
+            moveToFrame(getCurrentFrameNumber()+1);
             break;
         }else if(first_argument.compare("newframe")==0){
             std::cout << "Adding new frame."<< std::endl;
             addFrame(second_argument);//second_argument should be time to complete the new frame. New frame should be identical to previous.
-            break;
-        }else if(first_argument.compare("play")==0){
-            std::cout << "Playing script starting from the current frame."<< std::endl;
-            playScript();
+            break;        
         }else if(first_argument.compare("seek")==0){
-            interpretSeekCommand(command.str());
+            interpretSeekCommand(second_argument);
+            break;
+        }else if(first_argument.compare("duration")==0){
+            setCurrentFrameDuration(second_argument);
             break;
         } else { //Otherwise we interpret it as a motor position request
-            std::cout << "Performing motor manipulation. "<<first_argument<<" "<<command.str()<< std::endl;
-            applyRequestToMotors(first_argument,command.str());
+            std::cout << "Performing motor manipulation. "<<command.str()<< std::endl;
+            applyRequestToMotors(command.str());
         }
     }
 }
@@ -126,10 +151,10 @@ void ScriptTunerState::addFrame(string argument){
 std::cout << "Oh wait this isn't implemented yet."<< std::endl;
 }
 
-void ScriptTunerState::interpretSeekCommand(string command){
+void ScriptTunerState::interpretSeekCommand(string frame_number_string){
     int frame_number;
     std::stringstream s;
-    s << command;
+    s << frame_number_string;
     if(!(s >>frame_number)){
         std::cout << "Please input an integer as the second argument."<< std::endl;
         return;
@@ -150,51 +175,93 @@ void ScriptTunerState::playScript(){
     std::cout << "Oh wait this isn't implemented yet."<< std::endl;
 }
 
-void ScriptTunerState::applyRequestToMotors(string motor_id_string,string other_parameters){
+void ScriptTunerState::applyRequestToMotors(string parameters){
     std::stringstream command;
-    command << other_parameters;
-    try {
-        int motor_id = string_id_to_int_id.at(motor_id_string); // map::at throws an out-of-range if no key (C++11 feature)
-        
-        int positional_change, gain_change;
-        if(command >> positional_change){
-            changeMotorPosition(motor_id, positional_change);
-            if(command >> gain_change){
-                changeMotorGain(motor_id, gain_change);
+    command << parameters;
+    
+    string motor_id_string;
+    command >> motor_id_string;
+        try {
+            int motor_id = string_id_to_int_id.at(motor_id_string); // map::at throws an out-of-range if no key (C++11 feature)
+            
+            float positional_change, gain_change;
+            if(command >> positional_change){
+                changeMotorPosition(motor_id, positional_change);
+                if(command >> gain_change){
+                    changeMotorGain(motor_id, gain_change);
+                }
+                return;
+            } 
+
+            string instruction;
+            command >> instruction;
+            if(instruction.compare("off")==0){
+                turnOffMotor(motor_id);
+                return;
+            }else if(instruction.compare("on")==0){
+                turnOnMotor(motor_id);
+                return;
             }
-            return;
-        } 
+            
+            std::cout<<"Invalid Manipulation. Try <Motor Name> (<\"on\"/\"off\">) or (<position_change> <gain_change>)."<<std::endl;
 
-        string instruction;
-        command >> instruction;
-        if(instruction.compare("on")){
-            turnOffMotor(motor_id);
-            return;
-        }else if(instruction.compare("off")){
-            turnOnMotor(motor_id);
-            return;
-        }
-        
-        std::cout<<"Invalid Manipulation. Try <Motor Name> (<\"on\"/\"off\">) or (<position_change> <gain_change>)"<<std::endl;
-
-    } catch (const std::out_of_range& e) {
-        std::cout << "There is no such motor. Please try again"<< std::endl;
-    }    
+        } catch (const std::out_of_range& e) {
+            std::cout << "There is no such motor. Try again."<< std::endl;
+        }    
 }
 
 
-void changeMotorPostition(int motor_id, float pos_change){
-    std::cout<< "Motor "<< motor_id << " changed by "<< pos_change << std::endl;
+void ScriptTunerState::changeMotorPosition(int motor_id, float pos_change){
+    //DEBUG TEXT:
+    std::cout<< "Motor "<< motor_id << " position changed by "<< pos_change <<" radians."<< std::endl;
 }
 
-void changeMotorGain(int motor_id, float gain_change){
-    std::cout<< "Motor "<< motor_id << " gain changed by"<< gain_change<< std::endl;
+void ScriptTunerState::changeMotorGain(int motor_id, float gain_change){
+    //DEBUG TEXT:
+    std::cout<< "Motor "<< motor_id << " gain changed by "<< gain_change<< std::endl;
 }
 
-void turnOffMotor(int motor_id){
-    std::cout<< "Motor "<< motor_id << " torque off."<< << std::endl;
+void ScriptTunerState::turnOffMotor(int motor_id){
+    //DEBUG TEXT:
+    std::cout<< "Motor "<< motor_id << " torque off."<< std::endl;
 }
 
-void turnOnMotor(int motor_id){
-    std::cout<< "Motor "<< motor_id << " torque on."<< << std::endl;
+void ScriptTunerState::turnOnMotor(int motor_id){
+    //DEBUG TEXT:
+    std::cout<< "Motor "<< motor_id << " torque on."<< std::endl;
+}
+    
+
+    
+int ScriptTunerState::getCurrentFrameNumber(){
+    return 1;
+}
+    
+int ScriptTunerState::totalNumberOfFrames(){
+    return 1;
+}
+    
+float ScriptTunerState::durationOfCurrentFrame(){
+    return 1.0;
+}
+
+bool ScriptTunerState::scriptIsActive(){
+    return true;
+}
+
+void ScriptTunerState::applyCurrentFrameToRobot(){
+
+}
+
+void setCurrentFrameDuration(string duration_string){
+    std::stringstream stream;
+    stream << duration_string;
+    int duration;
+    if(stream >> duration){
+        std::cout<< "Setting current frame duration to "<< duration <<"."<< std::endl;
+        //MAIN CODE HERE
+
+    } else {
+        std::cout<< "Invalid duration time." << std::endl;
+    }
 }
--- Behaviour/ScriptTuner/ScriptTunerStates.h
+++ Behaviour/ScriptTuner/ScriptTunerStates.h
@@ -65,39 +65,50 @@ class ScriptTunerState : public ScriptTunerSubState
 {
 public:
     std::map<string, int> string_id_to_int_id; 
+    string filename;
 
     ScriptTunerState(ScriptTunerProvider* provider);
 
     BehaviourState* nextState() {return m_provider->m_state;}
     void doState();
+
     void editCurrentFrame();
+
     /*! @brief Loads a motion script from a file into the member variables frames and times */
     bool loadScript(string filename);
+
      /*! @brief Applies frame, given by frames[frame], to robot.*/
     void applyFrameToRobot();
+
      /*! @brief Reads motor positions from robot and saves them to the current frame, frames[frame].
         Only save motor positions which have had torque off and then back on.*/
     void saveManuallyMovedMotors();
+
      /*! @brief Writes the frames to script file.*/
     void saveScriptToFile(string filename);
+
      /*! @brief Adds a new frame to the vector of frames.*/
     void addFrame(string argument);
+
     /*! @brief Moves to the frame indicated by the first argument of command string, if it is an integer.*/
     void interpretSeekCommand(string command);
+
     /*! @brief Moves script to a given frame if it is a valid frame number.*/
     void moveToFrame(int frame_number);
+
     /*! @brief Exit program and load a new script.*/
     void exitScript();
+
     /*! @brief Play the script from the current frame.*/
     void playScript();
 
     /*! @brief Applies positional or torque settings to motors.
         @param other_parameters = "<position> <gain>". If gain is not specified a default value is used.*/
-    void applyRequestToMotors(string motorID,string other_parameters);
+    void applyRequestToMotors(string parameters);
     
     /*! @brief Motor fine tuning.
         @param pos_change is change in angle in radians.*/
-    void changeMotorPostition(int motor_id,float pos_change);
+    void changeMotorPosition(int motor_id,float pos_change);
     
     /*! @brief Motor fine tuning.
         @param gain_change is change in gain.*/    
@@ -109,7 +120,26 @@ public:
     /*! @brief Motor turn on torque method.
          Also marks motor for saving with save manually moved motors.*/    
     void turnOnMotor(int motor_id);
+    
+    /*! @brief Returns the current frame number.
+    */
+    int getCurrentFrameNumber();
+    
+    /*! @brief Returns the total number of frames for the loaded script.
+    */    
+    int totalNumberOfFrames();
+    
+    /*! @brief Returns the time required for the current frame to complete.
+    */    
+    float timeForCompletionOfCurrentFrame();
 
+    /*! @brief Indicates state of script during editing.
+    */
+    bool scriptIsActive();
+    
+    /*! @brief Updates robot to have motor positions indicated by the current frame in the current script.
+    */
+    void applyCurrentFrameToRobot();
 };
 
 #endif
