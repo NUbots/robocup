f5a3b70df12e97e46e7f8e205a55ac25e826e4e1
Mitchell Metcalfe <mitchell.s.metcalfe@gmail.com>
2013-06-26 17:13:54 +1000

    Fix MotionScript2013 compile error.

--- Motion/Kicks/ScriptKick2013.cpp
+++ Motion/Kicks/ScriptKick2013.cpp
@@ -200,7 +200,7 @@ float ScriptKick2013::GetCurrentScriptTime()
 {
     if(current_script_ == nullptr)
     {
-        std::cout << __PRETTY_FUNCTION__ << ": WARNING: current_script_ == nullptr;"
+        std::cout << __PRETTY_FUNCTION__ << ": WARNING: current_script_ == nullptr;" << std::cout;
         return 0;
     } 
 
